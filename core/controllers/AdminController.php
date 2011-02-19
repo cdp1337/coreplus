ed5ce7aad7ea34ae4d1981787c749461f36e1f6d
Charlie Powell <powellc@powelltechs.com>
2011-02-19 21:44:44 +0000

    Rebuilt the core component with the updates to the File systems. New stub Admin page, ReinstallAll

--- core/controllers/AdminController.php
+++ core/controllers/AdminController.php
@@ -6,5 +6,19 @@ class AdminController extends Controller {
 		$page->assignVariable('pages', $pages);
 	}
 	
+	public static function ReinstallAll(View $page){
+		// Just run through every component currently installed and reinstall it.
+		// This will just ensure that the component is up to date and correct as per the component.xml metafile.
+		
+		foreach(ComponentHandler::GetAllComponents() as $c){
+			if(!$c->isInstalled()) continue;
+			
+			$c->reinstall();
+		}
+		
+		// @todo Have some feedback or notification to the user...
+		// @todo Make the template too....
+	}
+	
 	//public static function Edit()
 }
