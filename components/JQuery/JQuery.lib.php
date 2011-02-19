f5b03cc72e654dc5ecf64ef2b887332c26140829
Charlie Powell <powellc@powelltechs.com>
2011-02-19 21:34:34 +0000

    Adding jQuery 1.5 as a component

---
+++ components/JQuery/JQuery.lib.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of JQuery
+ *
+ * @author powellc
+ */
+abstract class JQuery {
+	
+	public static function IncludeJQuery(){
+		if(ConfigHandler::GetValue('/jquery/minified')) CurrentPage::AddScript ('js/jquery/jquery-1.5.min.js');
+		else CurrentPage::AddScript ('js/jquery/jquery-1.5.js');
+	}
+	
+	public static function IncludeJQueryUI(){
+		
+	}
+}
+
+?>
