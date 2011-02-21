5f26346185b52465852e674b2db04ac97a55eda5
Charlie Powell <powellc@powelltechs.com>
2011-02-21 07:10:33 +0000

    Add LEFT JOIN support to the SQLBuilderSelect class.

--- core/libs/core/SQLBuilder.class.php
+++ core/libs/core/SQLBuilder.class.php
@@ -93,11 +93,18 @@ class SQLBuilderSelect extends SQLBuilder{
 	protected $_selects = array();
 
 	protected $_order = array();
+	
+	protected $_joins = array();
 
 	public function query(){
 		$q = "SELECT " . implode(', ', $this->_selects);
 		$q .= " FROM " . implode(', ', $this->_tables);
 		if(!empty($this->_wheres)) $q .= ' WHERE ' . implode(' AND ', $this->_wheres);
+		if(!empty($this->_joins)){
+			foreach($this->_joins as $j){
+				$q .= ' ' . $j['type'] . ' ' . $j['table'] . ' ON ' . ((is_array($j['on']))? implode(' AND ', $j['on']) : $j['on']);
+			}
+		}
 		if(!empty($this->_order)) $q .= ' ORDER BY ' . implode(', ', $this->_order);
 		if($this->_limit) $q .= ' LIMIT ' . $this->_limit;
 		return $q;
@@ -112,6 +119,47 @@ class SQLBuilderSelect extends SQLBuilder{
 
 		// Allow chaining
 		return $this;
+	}
+	
+	public function order($order){
+		if(strpos($order, ',') !== false) $order = explode(',', $order);
+		if(!is_array($order)) $order = array($order);
+		
+		$this->_order = array_merge($this->_order, $order);
+	}
+	
+	public function join($direction, $table, $on){
+		$j = array();
+		$j['type'] = strtoupper($direction) . ' JOIN';
+		$j['table'] = $table;
+		if(is_array($on)){
+			// @todo Finish this if needed.
+			/*
+			$j['on'] = array();
+			foreach($on as $k => $v){
+				
+				if(is_numeric($k)){
+					$j['on'][] = $v;
+				}
+				else{
+					// I need to ensure that table-including-keys remain formatted as `tblblah`.`colblah`
+					if(strpos($k, '.') !== false) $k = "`" . str_replace(".", "`.`", str_replace("`", "", $k)) . "`";
+					else $k = "`$k`";
+					
+					$j['on'][] = "$k = " . DB::qstr ($v);
+				}
+			}
+			*/
+		}
+		else{
+			$j['on'] = $on;
+		}
+		
+		$this->_joins[] = $j;
+	}
+	
+	public function leftJoin($table, $on){
+		$this->join('left', $table, $on);
 	}
 
 }
