224d6d521799c1cd810cb8bfd09f63b52e98f997
Charlie Powell <powellc@powelltechs.com>
2011-03-23 02:18:09 +0000

    Adding JQuery-Full, which includes jQuery 1.5.1, jqueryui 1.8.11, and any core ui plugins that are useful.

---
+++ components/JQuery-Full/JQuery.lib.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of JQuery
+ *
+ * @author powellc
+ */
+abstract class JQuery {
+	
+	public static function IncludeJQuery(){
+		if(ConfigHandler::GetValue('/jquery/minified')) CurrentPage::AddScript ('js/jquery/jquery-1.5.1.min.js');
+		else CurrentPage::AddScript ('js/jquery/jquery-1.5.1.js');
+	}
+	
+	public static function IncludeJQueryUI(){
+		self::IncludeJQuery();
+		CurrentPage::AddScript ('js/jquery/jquery-ui-1.8.11.min.js');
+		CurrentPage::AddStylesheet('css/jquery-ui.css');
+	}
+	
+	public static function Include_nestedSortable(){
+		$base = 'jquery.ui.nestedSortable';
+		// I need jquery ui first.
+		self::IncludeJQueryUI();
+		
+		if(ConfigHandler::GetValue('/jquery/minified')) CurrentPage::AddScript ('js/jquery/' . $base . '.min.js');
+		else CurrentPage::AddScript ('js/jquery/' . $base . '.js');
+	}
+}
+
+?>
