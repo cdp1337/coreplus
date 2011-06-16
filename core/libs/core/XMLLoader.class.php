fe47112b8b728e6d072a6e03706e10943d9559ab
powellc <powellc@powelltechs.com>
2011-06-16 02:05:09 -0400

    Ignore .DS_Store files. Switched most files in the Core to use LGPL v3.  This license allows for a little more flexibility when creating applications around the core and its components.

--- core/libs/core/XMLLoader.class.php
+++ core/libs/core/XMLLoader.class.php
@@ -1,4 +1,17 @@
-<?php 
+<?php
+/**
+ * // enter a good description here
+ * 
+ * @package Core
+ * @since 2011.06
+ * @author Charlie Powell <powellc@powelltechs.com>
+ * @copyright Copyright 2011, Charlie Powell
+ * @license GNU Lesser General Public License v3 <http://www.gnu.org/licenses/lgpl-3.0.html>
+ * This system is licensed under the GNU LGPL, feel free to incorporate it into
+ * custom applications, but keep all references of the original authors intact,
+ * read the full license terms at <http://www.gnu.org/licenses/lgpl-3.0.html>, 
+ * and please contribute back to the community :)
+ */
 
 /**
  * The XMLLoader object, capable of easily loading and saving XML data documents.
