96002f1b76c1c4f3754721e7de2f8e09f1783070
Nicholas Hinsch <hinschn@gmail.com>
2011-06-15 23:27:29 -0400

    Added JPlayer component

---
+++ components/jPlayer/jPlayer.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+abstract class jPlayer {
+	public static function IncludeJPlayer(){
+		ComponentHandler::LoadScriptLibrary('jquery');
+		CurrentPage::AddScript('js/jquery.jplayer.min.js');
+		CurrentPage::AddStylesheet('skin/jplayer.blue.monday.css');
+	
+		//Can has include?
+		return true;
+	}
+	
+}
+
+
+?>
