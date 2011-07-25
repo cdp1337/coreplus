edcf0493a89989c1d475e8c374578bb79ef0dfc0
cdp1337 <powellc@powelltechs.com>
2011-07-24 20:19:12 -0400

    Sessions are functional again! Signed-off-by:cdp1337 <powellc@powelltechs.com>

--- core/libs/core/Session.class.php
+++ core/libs/core/Session.class.php
@@ -16,25 +16,118 @@
 //This is a hack to allow classes to still be available after the page has been rendered.
 register_shutdown_function("session_write_close");
 
-/**
- * Kudos to Rich Smith <http://www.jamsoft.biz/> for the original concept I found
- *  a while back.
- * He submitted a script onto Devshed <http://www.devshed.com>
- * labled "Storing PHP Sessions in a Database" 
- * <http://www.devshed.com/c/a/PHP/Storing-PHP-Sessions-in-a-Database/>.
- *
- */
+
 class Session implements ISingleton{
+	
+	/**
+	 * The amodel for this given session.
+	 * 
+	 * @var SessionModel
+	 */
+	private $_model = null;
   
-	private $_sid;
-	private $_uid = 0;
-	private $_ttl;
+	private static $_Instance = null;
   
-	private static $instance = null;
+	/**
+	 * Get the global instance for the session.
+	 * 
+	 * @return Session
+	 */
+	public static function Singleton(){
+		if(self::$_Instance === null){
+			self::$_Instance = new Session();
+			
+			// Now I can session_start everything.
+			session_start();
+			
+			// And start a new session.
+			$m = self::$_Instance->_getModel();
+			// The "updated" flag is explictly required to be set because it also
+			// indicates when the last page activity for the user was.
+			// This is the main tracking variable for inactivity.
+			$m->set('updated', Time::GetCurrentGMT());
+			
+			// From this point on, the $_SESSION variable is available.
+			
+			// Anything waiting for the session?
+			HookHandler::DispatchHook('session_ready');
+		}
+
+		return Session::$_Instance;
+	}
+
+	public static function GetInstance(){
+		return Session::Singleton();
+	}
+
+	public static function Start($save_path, $session_name) {
+		// Singleton will handle starting a new session!
+		self::Singleton();
+	}
+
+	public static function End() {
+		// Nothing needs to be done in this function
+		// since we used persistent connection.
+	}
+
+	public static function Read( $id ) {
+		return self::Singleton()->_getModel()->get('data');
+	}
+
+	public static function Write( $id, $data ) {
+		
+		$m = self::Singleton()->_getModel();
+		$m->set('data', $data);
+		$m->save();
+		
+		return TRUE;
+	}
+
+	public static function Destroy( $id = null) {
+
+		// If "null" is requested, just destroy the system session.
+		if($id === null){
+			self::Singleton()->_destroy();
+		}
+		else{
+			// Low-level datasets are used here because they have less overhead than
+			// the full-blown model system.
+			$dataset = new Dataset();
+			$dataset->table('session');
+			$dataset->where('session_id = ' . $id );
+
+			$dataset->delete();
+		}
+		
+		return TRUE;
+	}
+
+	public static function GC() {
+		/**
+		 * Delete ANY session that has expired.
+		 */
+		$ttl = ConfigHandler::GetValue('/core/session/ttl');
+		
+		// Low-level datasets are used here because they have less overhead than
+		// the full-blown model system.
+		$dataset = new Dataset();
+		$dataset->table('session');
+		$dataset->where('updated < ' . (Time::GetCurrentGMT() - $ttl) );
+		
+		$dataset->delete();
+		
+		// Always return TRUE
+		return true;
+	}
+	
+	public static function SetUser(UserModel $u){
+		$m = self::Singleton()->_getModel();
+		
+		$m->set('user_id', $u->get('id'));
+		$_SESSION['user'] = $u;
+	}
   
 	private function __construct(){
-
-		$this->_ttl = ConfigHandler::GetValue('session', 'ttl');
 
 		// Set the save handlers
 		session_set_save_handler(
@@ -45,136 +138,27 @@ class Session implements ISingleton{
 			array('Session', "Destroy"),
 			array('Session', "GC")
 		);
-
-		// Possibly give a notice or something if it's started previously... or maybe not, shrug.
-    
+		
+		// Ensure garbage collection is done at some point.
+		Session::GC();
 	}
-  
-	public static function Singleton(){
-		if(is_null(Session::$instance)){
-			Session::$instance = new Session();
-
-			// Ensure garbage collection is done at some point.
-			Session::GC();
+	
+	private function _getModel(){
+		if($this->_model === null){
+			$this->_model = new SessionModel(session_id());
+			
+			// Ensure the data is matched up.
+			$this->_model->set('ip_addr', REMOTE_IP);
 		}
-
-		return Session::$instance;
+		
+		return $this->_model;
 	}
-
-	public static function GetInstance(){
-		return Session::Singleton();
-	}
-
-	public static function Start($save_path, $session_name) {
-		Session::Singleton()->_sid = session_id();
-
-		//This is a core PHP function.  Save that a session has been started in the default server log.
-		//error_log('Starting Session ' . $session_name . " ". $this->sid);//DEBUG//
-
-
-		/*
-		* Get the userID of the saved session (if exists).
-		* If the query returns no valid rows (ie: this is a NEW session),
-		* $data will be a blank array, thus never tripping the foreach and preserving $this->uid as FALSE.
-		*/
-		$rs = DB::Execute(
-			"SELECT `uid`
-			FROM ".DB_PREFIX."session
-			WHERE `sid` = ? AND `ip_addr` = ? LIMIT 1",
-			array(Session::Singleton()->_sid, REMOTE_IP)
-		);
-
-		$data = $rs->fields;
-		if(!$data) echo DB::Error ();
-		var_dump($rs, $data);
-		/**
-		* The session is NEW.  Create it.
-		*/
-		if($rs->NumRows() == 0){
-			echo "NEW";
-			DB::Execute(
-			"INSERT INTO ".DB_PREFIX."session
-			(`sid`, `ip_addr`, `uid`, `expires`)
-			VALUES
-			(?, ?, ?, ?)",
-			array(Session::Singleton()->_sid, REMOTE_IP, 0, Session::Singleton()->_ttl + Time::GetCurrent()));
+	
+	private function _destroy(){
+		// Delete the information from the database and purge the cached information.
+		if($this->_model){
+			$this->_model->delete();
+			$this->_model = null;
 		}
-		/**
-		* The session already exists, just update the timestamp.
-		*/
-		else{
-			Session::Singleton()->_uid = $rs->fields['uid'];
-
-			DB::Execute(
-			"UPDATE ".DB_PREFIX."session
-			SET `expires` = ?
-			WHERE `sid` = ? AND `ip_addr` = ?",
-			array(Session::Singleton()->_ttl + Time::GetCurrent(), Session::Singleton()->_sid, REMOTE_IP)
-			);
-		}
-	}
-
-	public static function End() {
-		// Nothing needs to be done in this function
-		// since we used persistent connection.
-	}
-
-	public static function Read( $id ) {
-
-		// Fetch session data from the selected database    
-		$data = DB::Execute("SELECT `session_data` FROM ".DB_PREFIX."session WHERE `sid` = '$id' AND `ip_addr` = '" . REMOTE_IP . "'");
-		$data = $data->fields['session_data'];
-
-		if(!$data) $data = '';
-
-		return $data;
-	}
-
-	public static function Write( $id, $data ) {
-		//CAELogger::write('Writing to the session...' . $data, 'debug', 'debug');
-
-		DB::Execute(
-		"UPDATE ".DB_PREFIX."session SET session_data = ? WHERE `sid` = ? AND `ip_addr` = ?",
-		array($data, $id, REMOTE_IP)
-		);
-
-		return TRUE;
-	}
-
-	public static function Destroy( $id = null) {
-
-		if(is_null($id)) $id = Session::$sid;
-		// Build query
-		DB::Execute("DELETE FROM `".DB_PREFIX."session` WHERE `sid` = '$id'");
-
-		return TRUE;
-	}
-
-	public static function GC() {
-		/**
-		* Delete ANY session that has expired.
-		*/
-		DB::Execute("DELETE FROM " . DB_PREFIX . "session WHERE `expires` < ?", array(Time::GetCurrent()));
-
-		// Always return TRUE
-		return true;
-	}
-
-	/**
-	* Saves the userID in the session database for logging in (or out) the user.
-	*
-	* @param int $uid
-	*/
-	public static function SetUID($uid){
-		DB::Execute(
-		"UPDATE `".DB_PREFIX."session`
-		SET `uid` = ?
-		WHERE `sid` = ? AND `ip_addr` = ?
-		LIMIT 1 ;",
-		array($uid, Session::Singleton()->_sid, REMOTE_IP)
-		);
 	}
 }
-
-//HookHandler::RegisterHook('libraries_loaded', 'Session::singleton');
-?>
---
+++ core/libs/core/SessionModel.class.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * // enter a good description here
+ * 
+ * @package Core
+ * @since 2011.06
+ * @author Charlie Powell <powellc@powelltechs.com>
+ * @copyright Copyright 2011, Charlie Powell
+ * @license GNU Lesser General Public License v3 <http://www.gnu.org/licenses/lgpl-3.0.html>
+ * This system is licensed under the GNU LGPL, feel free to incorporate it into
+ * custom applications, but keep all references of the original authors intact,
+ * read the full license terms at <http://www.gnu.org/licenses/lgpl-3.0.html>, 
+ * and please contribute back to the community :)
+ */
+
+
+/**
+ * Model for SessionModel
+ * 
+ * Generated automatically from the mysql_model_gen script.
+ * Please update result to your preferences and copy to the final location.
+ * 
+ * @author Charlie Powell <powellc@powelltechs.com>
+ * @date 2011-07-24
+ */
+class SessionModel extends Model {
+	public static $Schema = array(
+		'session_id' => array(
+			'type' => Model::ATT_TYPE_STRING,
+			'maxlength' => 256,
+			'required' => true,
+			'null' => false,
+		),
+		'user_id' => array(
+			'type' => Model::ATT_TYPE_INT,
+			'default' => 0,
+		),
+		'ip_addr' => array(
+			'type' => Model::ATT_TYPE_STRING,
+			'maxlength' => 39,
+		),
+		'data' => array(
+			'type' => Model::ATT_TYPE_TEXT,
+			'default' => null,
+			'null' => true,
+		),
+		'created' => array(
+			'type' => Model::ATT_TYPE_CREATED
+		),
+		'updated' => array(
+			'type' => Model::ATT_TYPE_UPDATED
+		)
+	);
+	
+	public static $Indexes = array(
+		'primary' => array('session_id'),
+	);
+
+	// @todo Put your code here.
+
+} // END class ConfigModel extends Model
