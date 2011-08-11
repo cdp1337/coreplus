779f05e4ac56b8614bd137601a593e1f3e65191d
cdp1337 <powellc@powelltechs.com>
2011-08-10 22:07:47 -0400

    Update for the User system. Added a "CheckEmailValidity" method in the core.  This should be a rather authoritative validation function... [check it out if intrigued ;) ] Signed-off-by:cdp1337 <powellc@powelltechs.com>

--- core/libs/core/Core.class.php
+++ core/libs/core/Core.class.php
@@ -244,7 +244,7 @@ class Core implements ISingleton{
 		if(self::$_User === null){
 			// Is the session data present?
 			if(!isset($_SESSION['user'])){
-				self::$_User = new UserModel();
+				self::$_User = new User();
 			}
 			else{
 				self::$_User = $_SESSION['user'];
@@ -733,6 +733,72 @@ class Core implements ISingleton{
 		if(strpos($str, '.') === false) return '';
 		
 		return substr($str, strrpos($str, '.') + 1 );
+	}
+	
+	/**
+	 * Validate an email address.
+	 * Provide email address (raw input)
+	 * Returns true if the email address has the email 
+	 * address format and the domain exists.
+	 * 
+	 * Copied (almost) verbatim from http://www.linuxjournal.com/article/9585?page=0,3
+	 * @author Douglas Lovell @ Linux Journal
+	 * 
+	 * @return boolean
+	 */
+	public static function CheckEmailValidity($email){
+		$atIndex = strrpos($email, "@");
+		if (is_bool($atIndex) && !$atIndex) return false;
+
+		$domain = substr($email, $atIndex+1);
+		$local = substr($email, 0, $atIndex);
+		$localLen = strlen($local);
+		$domainLen = strlen($domain);
+		if ($localLen < 1 || $localLen > 64) {
+			// local part length exceeded
+			return false;
+		}
+		
+		if ($domainLen < 1 || $domainLen > 255) {
+			// domain part length exceeded
+			return false;
+		}
+		
+		if ($local[0] == '.' || $local[$localLen-1] == '.') {
+			// local part starts or ends with '.'
+			return false;
+		}
+		
+		if (preg_match('/\\.\\./', $local)) {
+			// local part has two consecutive dots
+			return false;
+		}
+		if (!preg_match('/^[A-Za-z0-9\\-\\.]+$/', $domain)) {
+			// character not valid in domain part
+			return false;
+		}
+		
+		if (preg_match('/\\.\\./', $domain)) {
+			// domain part has two consecutive dots
+			return false;
+		}
+		
+		if (!preg_match('/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/', str_replace("\\\\","",$local))) {
+			// character not valid in local part unless local part is quoted
+			if (!preg_match('/^"(\\\\"|[^"])+"$/', str_replace("\\\\","",$local))) {
+				return false;
+			}
+		}
+		
+		// @todo Should these be disable-able from a config option?
+		//       possible reason for this is if the server does not have internet access.
+		if (!(checkdnsrr($domain,"MX") || checkdnsrr($domain,"A"))) {
+			// domain not found in DNS
+			return false;
+		}
+		
+		// All checks passed?
+		return true;
 	}
 	
 }
