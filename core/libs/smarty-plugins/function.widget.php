da532a62898b93138002d5357ee907e5209b51bb
cdp1337 <powellc@powelltechs.com>
2011-08-15 17:10:01 -0400

    Reworked the {widget} smarty plugin to work with the new widget structure. Signed-off-by:cdp1337 <powellc@powelltechs.com>

--- core/libs/smarty-plugins/function.widget.php
+++ core/libs/smarty-plugins/function.widget.php
@@ -1,26 +1,26 @@
 <?php
 
+/**
+ * Manually add a widget onto a template.
+ * 
+ * @param array $params
+ * @param Object $template
+ * @return mixed 
+ */
 function smarty_function_widget($params, $template){
 	
 	$name = $params['name'];
+	$assign= (isset($params['assign']))? $params['assign'] : false;
 	
-	$model = new WidgetModel($name);
-	//$model = new PageModel($name);
-	$out = $model->execute();
-	//$out->mode = View::MODE_WIDGET;
-	//var_dump($out); return;
-	return $out->render();
-	var_dump($out);
-	//var_dump($name, $model, WidgetModel::SplitBaseURL($name));
-	return '';
+	// Try to look up this requested widget.
+	$name .= 'Widget';
+	if(!class_exists($name)){
+		throw new SmartyException('Unable to locate class [' . $name . '] for requested widget', null, null);
+	}
+	// @todo Add support for requiring instancing.
 	
-	// I don't really care what it's called!
-	if(isset($params['file'])) $file = $params['file'];
-	elseif(isset($params['src'])) $file = $params['src'];
-	elseif(isset($params['href'])) $file = $params['href'];
+	$w = new $name();
+	$dat = $w->execute()->fetch();
 	
-	$f = Core::ResolveAsset($file);
-	
-	if(isset($params['assign'])) $template->assign($params['assign'], $f);
-	else return $f;
+	return $assign ? $template->assign($assign, $dat) : $dat;
 }
