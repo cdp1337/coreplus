a76b544d2ce6c1b1f18a2b08ec53eafbda9afa36
Charlie Powell <charlie@eval.bz>
2012-07-21 18:51:01 -0400

    Updated tinymce to newest stable version, 3.5.5

---
+++ components/tinymce/CHANGELOG.md
@@ -0,0 +1,4 @@
+3.5.5-core1
+2012.07.21
+Upgraded TinyMCE to newest upstream version.
+Removed several plugin icons, such as print and help.
--- components/tinymce/TinyMCE.lib.php
+++ components/tinymce/TinyMCE.lib.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * DESCRIPTION
+ * Handles all the integration of the TinyMCE plugin.
  *
- * @package
+ * @package TinyMCE
  * @since 0.1
  * @author Charlie Powell <charlie@eval.bz>
  * @copyright Copyright (C) 2009-2012  Charlie Powell
@@ -22,13 +22,13 @@
  */
 
 abstract class TinyMCE {
-	
+
 	public static function IncludeTinyMCE(){
 		ComponentHandler::LoadScriptLibrary('jquery');
 		CurrentPage::AddScript('js/tiny_mce/jquery.tinymce.js');
-		
+
 		$loc = Core::ResolveAsset('js/tiny_mce/tiny_mce.js');
-		
+
 		$script = <<< EOD
 <script type="text/javascript">
 	$(function(){
@@ -41,10 +41,10 @@ abstract class TinyMCE {
 			plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",
 
 			// Theme options
-			theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,/*styleselect,*/formatselect,fontselect,fontsizeselect,|,forecolor,backcolor",
+			theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect,fontselect,fontsizeselect,|,forecolor,backcolor",
 			theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,|,insertdate,inserttime",
-			theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl",
-			theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,/*template,*/pagebreak,|,preview,fullscreen,cleanup,code,|,help",
+			theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr",
+			theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,pagebreak,|,preview,fullscreen,cleanup,code",
 			theme_advanced_toolbar_location : "top",
 			theme_advanced_toolbar_align : "left",
 			theme_advanced_statusbar_location : "bottom",
@@ -72,7 +72,7 @@ abstract class TinyMCE {
 EOD;
 		// Add the necessary script
 		CurrentPage::AddScript($script, 'foot');
-		
+
 		// IMPORTANT!  Tells the script that the include succeeded!
 		return true;
 	}
