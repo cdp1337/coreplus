2e3f2fdd0929a210affd2000f9b80020f0cfaeb5
Charlie Powell <charlie@eval.bz>
2012-08-01 03:05:41 -0400

    Slight modifications to the user component
    Added some defaults to the user form accessstring.
    Made the email entry obtain focus automatically upon going to /user/login.

--- components/user/UserForm.class.php
+++ components/user/UserForm.class.php
@@ -24,12 +24,19 @@
 /**
  * Handles the form element for access strings.
  */
-class FormAccessStringInput extends FormElement{
+class FormAccessStringInput extends FormElement {
 	/**
 	 * Holds the base name of this form group
 	 * @var string
 	 */
 	private $_targetname = null;
+
+	public function __construct($atts = null) {
+		parent::__construct($atts);
+
+		// Some defaults
+		$this->_attributes['class'] = 'formelement formaccessstringinput formradioinput';
+	}
 
 	/**
 	 * Standard render function for this form element
@@ -38,77 +45,80 @@ class FormAccessStringInput extends FormElement{
 	 */
 	public function render() {
 		static $renderedcount = 0;
-		
+
 		$renderedcount++;
 		$this->_targetname = '_formaccessstring' . $renderedcount . \Core\RandomHex(8);
-		
-		$v = trim($this->get('value'));
-		$checked = 'advanced';
+
+		$v               = trim($this->get('value'));
+		$checked         = 'advanced';
 		$advanced_groups = array();
-		$type = 'whitelist';
-		
-		if($v == '*'){
+		$type            = 'whitelist';
+
+		if ($v == '*') {
 			$checked = 'basic_anyone';
-		}
-		elseif($v == '!*'){
+		} elseif ($v == '!*') {
 			$checked = 'basic_admin';
 		}
-		elseif(!$v){
+		elseif (!$v) {
 			// Blank value
 			$checked = 'advanced';
 		}
-		elseif($v == 'g:anonymous'){
+		elseif ($v == 'g:anonymous') {
 			$checked = 'basic_anonymous';
 		}
-		elseif($v == 'g:authenticated'){
+		elseif ($v == 'g:authenticated') {
 			$checked = 'basic_authenticated';
 		}
-        elseif($v == 'none'){
-            // Allow for a blank value.
-            $checked = null;
-            $type = null;
-        }
-		else{
+		elseif ($v == 'none') {
+			// Allow for a blank value.
+			$checked = null;
+			$type    = null;
+		}
+		else {
 			// Determine the sub groups checked.
 			$checked = 'advanced';
-			$parts = array_map('trim', explode(';', $v));
-			foreach($parts as $p){
-				if($p == '*'){
+			$parts   = array_map('trim', explode(';', $v));
+			foreach ($parts as $p) {
+				if ($p == '*') {
 					// If a wildcard is present, mark the groups as ones to blacklist.
 					$type = 'blacklist';
 					continue;
 				}
 				list($t, $tv) = explode(':', $p);
 				// Trim off the '!' in front of it, it'll be picked up by the presence of the '*' at the end.
-				if($tv{0} == '!') $tv = substr($tv, 1);
+				if ($tv{0} == '!') $tv = substr($tv, 1);
 				$advanced_groups[] = $tv;
 			}
 		}
-		
+
 		$groups = array();
-		
+
 		// Tack on the system groups.
 		$anongroup = new Model();
-		$anongroup->setFromArray(array(
-			'id' => 'anonymous',
-			'name' => 'Anonymous Users'
-		));
-		
+		$anongroup->setFromArray(
+			array(
+				'id'   => 'anonymous',
+				'name' => 'Anonymous Users'
+			)
+		);
+
 		$authgroup = new Model();
-		$authgroup->setFromArray(array(
-			'id' => 'authenticated',
-			'name' => 'Authenticated Users'
-		));
+		$authgroup->setFromArray(
+			array(
+				'id'   => 'authenticated',
+				'name' => 'Authenticated Users'
+			)
+		);
 		$groups[] = $anongroup;
 		$groups[] = $authgroup;
-		
+
 		// Find all the groups currently on the site.
 		$groups = array_merge($groups, UserGroupModel::Find(null, null, 'name'));
-		foreach($groups as $k => $v){
-			if(in_array($v->get('id'), $advanced_groups)) $v['checked'] = true;
+		foreach ($groups as $k => $v) {
+			if (in_array($v->get('id'), $advanced_groups)) $v['checked'] = true;
 		}
-		
-		
+
+
 		$tpl = new Template();
 		$tpl->assign('element', $this);
 		$tpl->assign('groups', $groups);
@@ -130,9 +140,9 @@ class FormAccessStringInput extends FormElement{
 	public function lookupValueFrom(&$src) {
 		// I'll let the parent do all the work.
 		$val = parent::lookupValueFrom($src);
-		
+
 		// In addition, I have to sift through the rest of the options for this system.
-		switch($val){
+		switch ($val) {
 			case 'basic_anyone':
 				return '*';
 			case 'basic_admin':
@@ -144,19 +154,19 @@ class FormAccessStringInput extends FormElement{
 			case '':
 				return null;
 		}
-		
+
 		// No?  alright, must be advanced...
-		if(!isset($src[$this->_targetname . '_type'])) $src[$this->_targetname . '_type'] = 'whitelist';
-		
-		$bool = ($src[$this->_targetname . '_type'] == 'whitelist') ? '' : '!';
+		if (!isset($src[$this->_targetname . '_type'])) $src[$this->_targetname . '_type'] = 'whitelist';
+
+		$bool   = ($src[$this->_targetname . '_type'] == 'whitelist') ? '' : '!';
 		$groups = array();
 		// Get each group selected
-		foreach($src[$this->_targetname] as $g){
+		foreach ($src[$this->_targetname] as $g) {
 			$groups[] = 'g:' . $bool . $g;
 		}
 		// And the white/black list itself.
-		if($src[$this->_targetname . '_type'] != 'whitelist') $groups[] = '*';
-		
+		if ($src[$this->_targetname . '_type'] != 'whitelist') $groups[] = '*';
+
 		return implode(';', $groups);
 	}
 }
--- components/user/templates/forms/elements/formaccessstringinput.tpl
+++ components/user/templates/forms/elements/formaccessstringinput.tpl
@@ -1,28 +1,27 @@
 {script library="jquery"}{/script}
 
 <div class="{$element->getClass()}">
-	{if $element->get('title')}
-		<label for="{$element->get('name')}">{$element->get('title')|escape}</label>
-	{/if}
-	
-	{if $element->get('description')}
-		<p class="formdescription">{$element->get('description')}</p>
-	{/if}
-	
-	<div class="formelement formradioinput">
+{if $element->get('title')}
+	<span>{$element->get('title')|escape}</span>
+{/if}
+
+{if $element->get('description')}
+	<p class="formdescription">{$element->get('description')}</p>
+{/if}
+	<div>
 		<label>
-			<input type="radio" name="{$element->get('name')}" class="{$dynname}_main" 
-				value="basic_anyone" {if $main_checked == 'basic_anyone'}checked="checked"{/if}/>
+			<input type="radio" name="{$element->get('name')}" class="{$dynname}_main"
+			       value="basic_anyone" {if $main_checked == 'basic_anyone'}checked="checked"{/if}/>
 			Allow Anyone
 		</label>
 		<label>
-			<input type="radio" name="{$element->get('name')}" class="{$dynname}_main" 
-				value="basic_anonymous" {if $main_checked == 'basic_anonymous'}checked="checked"{/if}/>
+			<input type="radio" name="{$element->get('name')}" class="{$dynname}_main"
+			       value="basic_anonymous" {if $main_checked == 'basic_anonymous'}checked="checked"{/if}/>
 			Only Anonymous
 		</label>
 		<label>
-			<input type="radio" name="{$element->get('name')}" class="{$dynname}_main" 
-				value="basic_authenticated" {if $main_checked == 'basic_authenticated'}checked="checked"{/if}/>
+			<input type="radio" name="{$element->get('name')}" class="{$dynname}_main"
+			       value="basic_authenticated" {if $main_checked == 'basic_authenticated'}checked="checked"{/if}/>
 			Only Authenticated
 		</label>
 		<label>
@@ -35,8 +34,8 @@
 	<fieldset style="display:none;">
 		<legend>
 			<label>
-				<input type="radio"name="{$element->get('name')}" class="{$dynname}_main" 
-					value="advanced" {if $advanced_type == 'advanced'}checked="checked"{/if}/>
+				<input type="radio" name="{$element->get('name')}" class="{$dynname}_main"
+				       value="advanced" {if $advanced_type == 'advanced'}checked="checked"{/if}/>
 				Advanced...
 			</label>
 		</legend>
@@ -45,33 +44,33 @@
 			<label><input type="radio" name="{$dynname}_type" value="blacklist"/>Disallow Only...</label>
 		</div>
 		<div class="formelement formcheckboxinput {$dynname}_advanced" style="display:none;">
-			{foreach from=$groups item='g'}
-				<label>
-					<input type="checkbox" name="{$dynname}[]" value="{$g->get('id')}" {if $g->get('checked')}checked="checked"{/if}/>{$g->get('name')}
-				</label>
-			{/foreach}
+		{foreach from=$groups item='g'}
+			<label>
+				<input type="checkbox" name="{$dynname}[]" value="{$g->get('id')}" {if $g->get('checked')}checked="checked"{/if}/>{$g->get('name')}
+			</label>
+		{/foreach}
 		</div>
 	</fieldset>
-		
+
 
 	<script type="text/javascript">
-		$(function(){
+		$(function () {
 			$('input[name="{$element->get('name')}"][value="advanced"]').closest('fieldset').show();
-			{if $advanced_type}
-				$('input[name={$dynname}_type][value={$advanced_type}]').click();
-			{/if}
-			
-			{if $main_checked}
-				$('input.{$dynname}_main[value={$main_checked}]').click();
-			{/if}
+		{if $advanced_type}
+			$('input[name={$dynname}_type][value={$advanced_type}]').click();
+		{/if}
+
+		{if $main_checked}
+			$('input.{$dynname}_main[value={$main_checked}]').click();
+		{/if}
 		});
-		$('.{$dynname}_main').click(function(){
+		$('.{$dynname}_main').click(function () {
 			var $this = $(this),
-				v = $this.val();
-			if(v == 'advanced'){
+					v = $this.val();
+			if (v == 'advanced') {
 				$('.{$dynname}_advanced').show();
 			}
-			else{
+			else {
 				$('.{$dynname}_advanced').hide();
 			}
 		});
