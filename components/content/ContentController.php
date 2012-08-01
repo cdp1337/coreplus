4315463e3afe50696089eb10a93275d4eda63335
Charlie Powell <charlie@eval.bz>
2012-08-01 02:58:47 -0400

    Sort content pages by nickname.

--- components/content/ContentController.php
+++ components/content/ContentController.php
@@ -30,7 +30,7 @@ class ContentController extends Controller_2_1 {
 			return View::ERROR_ACCESSDENIED;
 		}
 		
-		$f = ContentModel::Find(null, null, null);
+		$f = ContentModel::Find(null, null, 'nickname');
 		
 		$view->templatename = '/pages/content/index.tpl';
 		$view->title = 'Content Page Listings';
@@ -97,84 +97,83 @@ class ContentController extends Controller_2_1 {
 		}
 
 		$view->templatename = '/pages/content/edit.tpl';
-		$view->title = 'Edit ' . $m->get('title');
+		$view->title        = 'Edit ' . $m->get('title');
 		$view->assignVariable('model', $m);
 		$view->assignVariable('form', $form);
-		
-		if($manager) $view->addControl('Add Page', '/Content/Create', 'add');
+
+		if ($manager) $view->addControl('Add Page', '/Content/Create', 'add');
 		$view->addControl('View Page', '/Content/View/' . $m->get('id'), 'view');
-		if($manager) $view->addControl('Delete Page', '/Content/Delete/' . $m->get('id'), 'delete');
-		if($manager) $view->addControl('All Content Pages', '/Content', 'directory');
+		if ($manager) $view->addControl('Delete Page', '/Content/Delete/' . $m->get('id'), 'delete');
+		if ($manager) $view->addControl('All Content Pages', '/Content', 'directory');
 	}
 
-	public function create(){
-		
+	public function create() {
+
 		$view = $this->getView();
-		
-		if(!$this->setAccess('p:content_manage')){
+
+		if (!$this->setAccess('p:content_manage')) {
 			return View::ERROR_ACCESSDENIED;
 		}
-		
+
 		$m = new ContentModel();
-		
+
 		$form = Form::BuildFromModel($m);
 		$form->set('callsmethod', 'ContentController::_SaveHandler');
-		
+
 		$form->addElement('pagemeta', array('name' => 'page'));
-		
+
 		$form->addElement('pageinsertables', array('name' => 'insertables', 'baseurl' => '/Content/View/new'));
-		
+
 		// Tack on a submit button
 		$form->addElement('submit', array('value' => 'Create'));
 
-		
+
 		$view->templatename = '/pages/content/create.tpl';
-		$view->title = 'New Content Page';
+		$view->title        = 'New Content Page';
 		$view->assignVariable('model', $m);
 		$view->assignVariable('form', $form);
-		
+
 		$view->addControl('All Content Pages', '/Content', 'directory');
 	}
-	
-	public static function _SaveHandler(Form $form){
-		
+
+	public static function _SaveHandler(Form $form) {
+
 		$model = $form->getModel();
 		// Ensure that everything is marked as updated...
 		$model->set('updated', Time::GetCurrent());
 		//var_dump($model); die();
 		$model->save();
-		
+
 		$page = $form->getElementByName('page')->getModel();
 		$page->set('baseurl', '/Content/View/' . $model->get('id'));
 		$page->set('updated', Time::GetCurrent());
 		$page->save();
-		
+
 		$insertables = $form->getElementByName('insertables');
 		$insertables->set('baseurl', '/Content/View/' . $model->get('id'));
 		$insertables->save();
-		
+
 		// w00t
 		return $page->getResolvedURL();
 	}
-	
-	public function delete(){
-		$view = $this->getView();
+
+	public function delete() {
+		$view    = $this->getView();
 		$request = $this->getPageRequest();
 
 		// This is a POST-only page.
-		if(!$request->isPost()){
+		if (!$request->isPost()) {
 			return View::ERROR_BADREQUEST;
 		}
 
-		if(!$this->setAccess('p:content_manage')){
+		if (!$this->setAccess('p:content_manage')) {
 			return View::ERROR_ACCESSDENIED;
 		}
 
 		$m = new ContentModel($request->getParameter(0));
 
-		if(!$m->exists()) return View::ERROR_NOTFOUND;
+		if (!$m->exists()) return View::ERROR_NOTFOUND;
 		$m->delete();
 		Core::Redirect('/content');
 	}
 }
-?>
