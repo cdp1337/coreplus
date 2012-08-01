f7fa31b0e9df546f7e5995a1830c5ef5d84a14b7
Charlie Powell <charlie@eval.bz>
2012-08-01 03:04:06 -0400

    New component - jquery-hoverintent

---
+++ components/jquery-hoverintent/JQueryHoverIntent.lib.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: hinschn
+ * Date: 7/24/12
+ * Time: 10:13 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class JQueryHoverIntent {
+    public static function Load(){
+
+        CurrentPage::AddScript ('js/jquery.hoverIntent.min.js');
+
+        // IMPORTANT!  Tells the script that the include succeeded!
+        return true;
+    }
+}
