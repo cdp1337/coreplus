e4ff7377af7e170c7d2828535fa8e9423de8187d
Charlie Powell <charlie@eval.bz>
2012-12-30 16:35:08 -0500

    Spelling fixes.

--- utilities/packager.php
+++ utilities/packager.php
@@ -86,9 +86,9 @@ $_cversions = null;
  * - Src:/home/test/file.txt ,Dst:/home/test/b ,Result:/home/test/b -> If source was file copy file.txt name with b as name to destination
  * - Src:/home/test/file.txt ,Dst:/home/test/b/ ,Result:/home/test/b/file.txt -> If source was file Creates b directory if does not exsits and copy file.txt into it
  * - Src:/home/test ,Dst:/home/ ,Result:/home/test/** -> If source was directory copy test directory and all of its content into dest
- * - Src:/home/test/ ,Dst:/home/ ,Result:/home/**-> if source was direcotry copy its content to dest
- * - Src:/home/test ,Dst:/home/test2 ,Result:/home/test2/** -> if source was directoy copy it and its content to dest with test2 as name
- * - Src:/home/test/ ,Dst:/home/test2 ,Result:->/home/test2/** if source was directoy copy it and its content to dest with test2 as name
+ * - Src:/home/test/ ,Dst:/home/ ,Result:/home/**-> if source was directory copy its content to dest
+ * - Src:/home/test ,Dst:/home/test2 ,Result:/home/test2/** -> if source was directory copy it and its content to dest with test2 as name
+ * - Src:/home/test/ ,Dst:/home/test2 ,Result:->/home/test2/** if source was directory copy it and its content to dest with test2 as name
  * @todo
  *     - Should have rollback technique so it can undo the copy when it wasn't successful
  *  - Auto destination technique should be possible to turn off
