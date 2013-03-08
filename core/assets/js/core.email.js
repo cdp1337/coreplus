1be4372e477c416306c97aa00acd153a57564e05
Charlie Powell <charlie@eval.bz>
2013-03-07 23:05:58 -0500

    New methods, a smarty {mail} function and the corresponding email javascript.

---
+++ core/assets/js/core.email.js
@@ -0,0 +1,34 @@
+/**
+ * Provides some string manipulations that can be used throughout.
+ */
+
+
+(function(){
+
+	Core.Email = {
+		/**
+		 * Cleanup a string and ensure it can make a valid URL.
+		 *
+		 * @param string
+		 * @return string
+		 */
+
+		assemble: function($node){
+
+			var id      = $node.attr('id'),
+				user    = Core.Strings.rot13( $node.attr('user') ),
+				domain  = $node.attr('domain'),
+				tld     = $node.attr('tld');
+
+			var address = user + '@' + domain + '.' + tld;
+
+			$node.html(address);
+		}
+
+	};
+
+	String.prototype.assemble = function(){
+		return Core.Strings.assemble(this.toString());
+	};
+
+})();
