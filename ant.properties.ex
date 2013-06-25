561cbb31e2eeda832dbce9e31233cedd4c7e3ea5
Charlie Powell <charlie@eval.bz>
2013-06-25 14:38:34 -0400

    Added support for an upstream project.
    The upstream variables has been set in ant.properties.ex, (for copying to ant.properties).
    New utility script, sync_from_upstream.  This will clone the upstream code to the local checkout.

--- ant.properties.ex
+++ ant.properties.ex
@@ -1,18 +1,59 @@
-# ${basedir} is an automatic variable set by the ant system.
+# Ant Properties Example Template
+#
+# Use this file as a template for your own "ant.properties" file for building the application with ant.
+#
+# (${basedir} is an automatic variable set by the ant system.)
 # It's default to the directory the build.xml file is located in.
 
 
 # Rewrite base for the site.
+#
 # If this is currently installed in /~userblah/coreplus, set this to
 # /~userblah/coreplus/
+#
+# This gets copied to the .htaccess file.
 rewritebase=/src/
 
 
-
+# Database server name or IP address
+# Gets copied to the config/configuration.xml file.
 db.server=localhost
+
+# Database port
+# Gets copied to the config/configuration.xml file.
 db.port=3306
+
+
+# Database type
+# Gets copied to the config/configuration.xml file.
 db.type=mysqli
+
+
+# Database name
+# Gets copied to the config/configuration.xml file.
 db.name=coreplus
+
+
+# Database user
+# Gets copied to the config/configuration.xml file.
 db.user=coreplus
+
+
+# Database password
+# Gets copied to the config/configuration.xml file.
 db.pass=coreplus
+
+
+# Controls if the site is in DEVELOPMENT mode or not.
+#
+# Setting this to "false" will be equivalent to production and "true" will be development.
+#
+# Gets copied to the config/configuration.xml file.
 devmode=false
+
+
+# Upstream directory
+#
+# If you have Core Plus checked out locally, enter the full path here.
+# This is used by the sync_from_upstream.sh script for importing changes in Core and the core components.
+upstream=
---
+++ utilities/sync_from_upstream.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+BASEDIR="$(readlink -f $(dirname $0)/..)"
+
+# Get the upstream source.
+UPSTREAM="$(cat "$BASEDIR/ant.properties" | egrep '^upstream=' | sed 's:^[^=]*=\(.*\):\1:')"
+
+if [ -z "$UPSTREAM" ]; then
+	echo "Please set the upstream directory in your properties.ant file!"
+	echo ""
+	echo "The line should look like:"
+	echo "upstream=/path/to/core/upstream"
+	echo ""
+	echo "Which points to the top-level directory containing utilities/, src/, etc."
+	exit 1
+fi
+
+if [ ! -e "$UPSTREAM" ]; then
+	echo "$UPSTREAM does not seem to exist!"
+	exit 1
+fi
+
+COMPONENTS="$(ls "$BASEDIR/src/components")"
+OPTIONS="-rag"
+
+echo "Syncing build scripts..."
+rsync $OPTIONS --delete "$UPSTREAM/build/" "$BASEDIR/build"
+rsync $OPTIONS "$UPSTREAM/build.xml" "$BASEDIR/"
+rsync $OPTIONS "$UPSTREAM/ant.properties.ex" "$BASEDIR/"
+rsync $OPTIONS "$UPSTREAM/build.xml" "$BASEDIR/"
+
+echo "Syncing vendor..."
+rsync $OPTIONS --delete "$UPSTREAM/vendor/" "$BASEDIR/vendor"
+
+echo "Syncing utilities..."
+rsync $OPTIONS "$UPSTREAM/utilities/" "$BASEDIR/utilities"
+
+echo "Syncing docs..."
+rsync $OPTIONS "$UPSTREAM/docs/" "$BASEDIR/docs"
+
+#rsync $OPTIONS "$UPSTREAM/exports/" "$BASEDIR/exports"
+
+
+echo "Syncing core..."
+rsync $OPTIONS --delete "$UPSTREAM/src/core/" "$BASEDIR/src/core"
+rsync $OPTIONS --delete "$UPSTREAM/src/install/" "$BASEDIR/src/install"
+rsync $OPTIONS "$UPSTREAM/src/config/configuration.xml.ex" "$BASEDIR/src/config/"
+rsync $OPTIONS "$UPSTREAM/src/htaccess.ex" "$BASEDIR/src/"
+
+echo "Syncing core themes..."
+#rsync $OPTIONS --delete "$UPSTREAM/src/themes/default/" "$BASEDIR/src/themes/default"
+rsync $OPTIONS --delete "$UPSTREAM/src/themes/base-v2/" "$BASEDIR/src/themes/base-v2"
+
+
+for i in $COMPONENTS; do
+	if [ -e "$UPSTREAM/src/components/$i" ]; then
+		echo "Syncing  component $i..."
+		rsync $OPTIONS --delete "$UPSTREAM/src/components/$i/" "$BASEDIR/src/components/$i"
+	else
+		echo "Skipping component $i, (does not exist in upstream)"
+	fi
+done
