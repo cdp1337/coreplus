edb31ff7d6c39f34cf6e32d532cab44a10abcfba
Charlie Powell <charlie@eval.bz>
2013-07-15 22:33:43 -0400

    Namespaces are now supported in the compiler!
    This allows more files to be combined into the compiled version, allowing for even more performance gains.  This will be rolled out in the 2.6.0 release.

--- utilities/compiler.php
+++ utilities/compiler.php
@@ -35,6 +35,30 @@ require_once(ROOT_PDIR . 'core/bootstrap.php');
 
 // Open the bootstrap.php file and read in the sourcecode of the requested files.
 
+class CompilerNamespace {
+	public $name = null;
+	public $isopen = false;
+
+	public function __construct($name = null){
+		$this->name = $name;
+	}
+
+	public function getOpeningTag(){
+		if($this->isopen) return '';
+
+		$this->isopen = true;
+		return 'namespace ' . $this->name . ' {' . "\n";
+	}
+
+	public function getClosingTag(){
+		if(!$this->isopen) return '';
+
+		$this->isopen = false;
+		$name = ($this->name) ? 'NAMESPACE ' . $this->name : 'GLOBAL NAMESPACE';
+		return '} // ENDING ' . $name . "\n";
+	}
+}
+
 /**
  * Will be a list of files that have already been included.
  * Since it is require_once... it'll check first.
@@ -42,7 +66,7 @@ require_once(ROOT_PDIR . 'core/bootstrap.php');
 global $included_files;
 $included_files = array();
 
-function compile_file($filename, $recursivelevel = 0){
+function compile_file($filename, $recursivelevel = 0, CompilerNamespace $parentnamespace){
 
 	global $included_files;
 
@@ -54,11 +78,17 @@ function compile_file($filename, $recursivelevel = 0){
 	echo "Scanning " . $filename . "...\n";
 	$fh = fopen($filename, 'r');
 	if(!$fh) die('Unable to open [' .$filename . '] for reading.');
-	$contents = '';
-	$linenumber = 0;
-	$codenumber = 0;
-	$incomment = false;
-	$lastwasending = false;
+
+	$contents       = '';
+	$linenumber     = 0;
+	$codenumber     = 0;
+	$incomment      = false;
+	$lastwasending  = false;
+	// Will get reset to an internal one if there is one defined.
+	$namespace      = $parentnamespace;
+	$namespacename  = null;
+	$namespacewrote = false;
+
 	while(!feof($fh)){
 		$line = trim(fgets($fh, 1024));
 		++$linenumber;
@@ -97,10 +127,26 @@ function compile_file($filename, $recursivelevel = 0){
 		// In a multiline comment?
 		if($incomment) continue;
 
-		// Namespaces invalidate the entire file.
-		if(strpos($line, 'namespace ') === 0) return false;
+		// Namespaces must be first in the code!
+		// If the parent namespace was defined and is identical,
+		// then don't worry about anything, as the parent will take care of opening and closing it.
+		if(strpos($line, 'namespace ') === 0){
+			$namespacename = preg_replace('#[^a-zA-Z\\\\]#', '', substr($line, 10));
+			// Was this the same namespace as the parent?  If so, I don't need to do anything.
+			if($namespacename != $parentnamespace->name){
+				// Close the previous namespace before I open a new one!
+				$contents .= $parentnamespace->getClosingTag();
+
+				// And start a new one.
+				$namespace = new CompilerNamespace($namespacename);
+				$contents .= $namespace->getOpeningTag();
+				++$codenumber;
+			}
+			continue;
+		}
 
 		// If recursion is enabled, we will recurse into REQUIRE_ONCE statements.
+		// And if the namespace is global....
 		if(strpos($line, 'require_once') === 0 && $recursivelevel > 0){
 			$subfile = preg_replace('#require_once[ ]*\([ ]*([^\)]*)[ ]*\)[ ]*;#', '$1', $line);
 			// The file probably has relative paths...
@@ -127,14 +173,17 @@ function compile_file($filename, $recursivelevel = 0){
 
 				eval("\$subfile = $subfile;");
 
-				$filecontents = compile_file($subfile, ($recursivelevel-1));
+				$filecontents = compile_file($subfile, ($recursivelevel-1), $namespace);
 
 				// Trim the root directory and put a location-independent one instead.
 				$subfilerelative = substr($subfile, strlen(ROOT_PDIR));
 
 				// If this file could not be minified, false will be returned.
 				if($filecontents !== false){
-					$line = '### REQUIRE_ONCE FROM ' . $subfilerelative . "\n" . $filecontents;
+					$line = '### REQUIRE_ONCE FROM ' . $subfilerelative . "\n" . $filecontents . "\n";
+					// Do I need to re-open my namespace?
+					// This doesn't actually have any effect if it's already open :)
+					$line .= $namespace->getOpeningTag();
 				}
 			}
 		}
@@ -148,12 +197,39 @@ function compile_file($filename, $recursivelevel = 0){
 		++$codenumber;
 		//if($codenumber % 10 == 0) $line = '## ' . basename($filename) . ':' . $linenumber . "\n" . $line;
 
+		// I need to make sure that a namespace was written already.
+		// This is because the namespace must be the first line in the document.
+		/*
+		if(!$namespacewrote){
+			// Was this the same namespace as the parent?  If so, I don't need to do anything.
+			if($namespace != $parentnamespace){
+				// Close the previous namespace before I open a new one!
+				$contents .= "} // ENDING NAMESPACE " . $parentnamespace . "\n";
+				$contents .= "namespace {\n";
+				++$codenumber;
+				$contents .= $line . "\n";
+				$namespacewrote = true;
+			}
+			$namespacewrote = true;
+		}
+		*/
 		$contents .= $line . "\n";
 	}
+
+	// Before I return, I need to check and see if the parent had a different namespace.
+	// If it did... I should probably close mine so the parent can reopen its namespace.
+	if($namespace->name != $parentnamespace->name){
+		$contents .= $namespace->getClosingTag();
+	}
+
+	echo "Finished scanning $filename, found $codenumber lines\n";
 	return $contents;
 }
 
-$contents = compile_file(ROOT_PDIR . 'core/bootstrap.php', 4);
+$globalnamespace = new CompilerNamespace();
+// Start the namespace.
+$contents = $globalnamespace->getOpeningTag();
+$contents .= compile_file(ROOT_PDIR . 'core/bootstrap.php', 4, $globalnamespace);
 
 // The compiled file will have a header stating some useful information.
 $date = Time::GetCurrent(Time::TIMEZONE_DEFAULT, Time::FORMAT_RFC2822);
@@ -171,8 +247,8 @@ $header = <<<EOD
  * @package Core Plus\Core
  * @since 2.1.5
  * @author Charlie Powell <charlie@eval.bz>
- * @copyright Copyright (C) 2009-2012  Charlie Powell
- * @license GNU Affero General Public License v3 <http://www.gnu.org/licenses/agpl-3.0.txt>
+ * @copyright Copyright (C) 2009-2013  Charlie Powell
+ * @license     GNU Affero General Public License v3 <http://www.gnu.org/licenses/agpl-3.0.txt>
  *
  * @compiled $date
  *
@@ -189,7 +265,40 @@ $header = <<<EOD
  * along with this program.  If not, see http://www.gnu.org/licenses/agpl-3.0.txt.
  */
 
+
+//===========================================================================\\\\
+//                                       _____________________________       \\\\
+//                                      |                             |      \\\\
+//                                      |  You're in the wrong file!  |      \\\\
+//                                      |_____    ____________________|      \\\\
+//                                            \  /                           \\\\
+//                                             \/                            \\\\
+//                                                                           \\\\
+//                                           _/|__                           \\\\
+//                       _,-------,        _/ -|  \_     /~>.                \\\\
+//                    _-~ __--~~/\ |      (  \   /  )   | / |                \\\\
+//                 _-~__--    //   \\\\      \ *   * /   / | ||                \\\\
+//              _-~_--       //     ||      \     /   | /  /|                \\\\
+//             ~ ~~~~-_     //       \\\\     |( " )|  / | || /                \\\\
+//                     \\   //         ||    | VWV | | /  ///                 \\\\
+//               |\\     | //           \\\\ _/      |/ | ./|                   \\\\
+//               | |    |// __         _-~         \// |  /                  \\\\
+//              /  /   //_-~  ~~--_ _-~  /          |\// /                   \\\\
+//             |  |   /-~        _-~    (     /   |/ / /                     \\\\
+//            /   /           _-~  __    |   |____|/                         \\\\
+//           |   |__         / _-~  ~-_  (_______  `\                        \\\\
+//           |      ~~--__--~ /  _     \        __\)))                       \\\\
+//            \               _-~       |     ./  \                          \\\\
+//             ~~--__        /         /    _/     |                         \\\\
+//                   ~~--___/       _-_____/      /                          \\\\
+//                    _____/     _-_____/      _-~                           \\\\
+//                 /^<  ___       -____         -____                        \\\\
+//                    ~~   ~~--__      ``\--__       ``\                     \\\\
+//                               ~~--\)\)\)   ~~--\)\)\)                     \\\\
+//                                                                           \\\\
+//===========================================================================\\\\
+
+
 EOD;
 
-
-file_put_contents(ROOT_PDIR . 'core/bootstrap.compiled.php', '<?php' . "\n" . $header . $contents);
+file_put_contents(ROOT_PDIR . 'core/bootstrap.compiled.php', '<?php' . "\n" . $header . $contents . $globalnamespace->getClosingTag());
