1174d5ac53a9395db159ba55b62e5ec7062ebe83
Charlie Powell <charlie@eval.bz>
2013-08-01 00:28:41 -0400

    Update the google js code to the newest version available.

--- src/components/google/libs/google/analytics/Helper.php
+++ src/components/google/libs/google/analytics/Helper.php
@@ -56,22 +56,29 @@ namespace Google\Analytics;
 abstract class Helper {
 	public static function InstallTracking(){
 		$gacode = \ConfigHandler::Get('/google-analytics/accountid');
-		$site = HOST;
-		$min = true;
+		//$site = HOST;
+		$min = \ConfigHandler::Get('/core/markup/minified');
 
 		// If there's no code available, don't display anything.
 		if(!$gacode) return;
 
-		// This version of the script is Google's newest version as of 2011.09.22
+		// This version of the script is Google's newest version as of 2013.07
 		$script = <<<EOD
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', '$gacode', '$site');
-  ga('send', 'pageview');
+		<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '$gacode']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+  })();
 
 </script>
 EOD;
