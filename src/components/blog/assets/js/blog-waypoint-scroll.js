0ab7d68d48ced312b4f65feaf6787c7d258199e6
Charlie Powell <charlie@eval.bz>
2013-08-06 20:34:49 -0400

    Fix the bug of waypoints not knowing where the reference point is.
    There was a bug with jquery waypoints where it didn't know where the DOM element was on the page after it was moved to after content.
    This is resolved by reinitializing the waypoints logic on pagination load.

--- src/components/blog/assets/js/blog-waypoint-scroll.js
+++ src/components/blog/assets/js/blog-waypoint-scroll.js
@@ -1,40 +1,59 @@
 // They generally start on page one.  However if the request had page=12, then start on that page!
-var currentpage = parseInt(Core.Request.get('page') || 1), loadingnow = null;
+var
+	currentpage = parseInt(Core.Request.get('page') || 1),
+	loadingnow = null,
+	scrollhandler = null,
+	$reftarget = $('#bottomofthelisting');
 
-$('#bottomofthelisting').waypoint({
-	offset: '105%',
-	handler: function(direction){
-		// Are there no more results?
-		if(currentpage == null) return;
+scrollhandler = function(direction){
+	// Are there no more results?
+	if(currentpage == null) return;
 
-		// If this page is currently loading, don't try to reload again!
-		if(loadingnow == currentpage) return;
+	// If this page is currently loading, don't try to reload again!
+	if(loadingnow == currentpage) return;
 
-		// yay, the user is scrolling down... load the next set of results!
-		currentpage += 1; // Increment
-		loadingnow = currentpage; // and remember
+	// If it's scrolling up... why do I care?
+	if(direction != 'down') return;
 
-		$.ajax({
-			url: '?page=' + currentpage,
-			success: function(data){
-				// Yay, it's loaded... I can clear the results.
-				loadingnow = null;
+	// yay, the user is scrolling down... load the next set of results!
+	currentpage += 1; // Increment
+	loadingnow = currentpage; // and remember
 
-				var $articles = $(data).find('.blog-article');
-				if($articles.length == 0){
-					// There aren't any more results :/
-					currentpage = null;
-					return;
-				}
+	$.ajax({
+		url: '?page=' + currentpage,
+		success: function(data){
+			// Yay, it's loaded... I can clear the results.
+			loadingnow = null;
 
-				// Append them!
-				$articles.each(function(){
-					$('.blog-listing').append(this);
-				});
+			// Clear out the waypoint... for some reason it's not picking up on the moved node after appending.
+			$reftarget.waypoint('destroy');
 
+			var $articles = $(data).find('.blog-article');
+			if($articles.length == 0){
+				// There aren't any more results :/
+				currentpage = null;
+				return;
 			}
-		});
-	}
+
+			// Append them!
+			$articles.each(function(){
+				$('.blog-listing').append(this);
+			});
+
+			// And now I can reinitialize the waypoints.
+			$reftarget.waypoint({
+				offset: '110%',
+				handler: scrollhandler
+			});
+
+		}
+	});
+};
+
+
+$reftarget.waypoint({
+	offset: '110%',
+	handler: scrollhandler
 });
 
 // And hide the pagination.
