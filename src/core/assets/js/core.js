23dc045a527119cbabf753f549cd3c1845267cd2
Charlie Powell <charlie@eval.bz>
2013-08-18 20:48:00 -0400

    Optimize some of the Core javascript a bit.
    In an ever-running attempt to eek more performance out of the system, core-foot.js and core.js have been merged into a single file.
    This reduces the server calls by one request.

--- src/core/assets/js/core.js
+++ src/core/assets/js/core.js
@@ -1,4 +1,9 @@
-// Core should already be defined.
+/**
+ * Any core javascript that needs to be in the foot of the document.
+ * This is just for core systems and functions; components and plugins can have your own file!
+ *
+ * Note, Core is already defined in the header.
+ */
 
 (function(){
 
@@ -100,11 +105,53 @@
 		window.location.reload();
 	};
 
+	/**
+	 * Define console as an object so console.logs don't break in IE.
+	 */
 	if(typeof console == 'undefined'){
 		console = {};
 	}
 	if(typeof console.log == 'undefined'){
 		console.log = function(){ /* This page purposefully left blank :p */ }
+	}
+
+	/**
+	 * Below are some jquery-specific functions, only applicable if jquery is loaded already.
+	 * This will NOT be required by default because jquery is not a requirement of Core,
+	 * simply a recommendation.
+	 */
+	if(typeof jQuery != 'undefined'){
+		// The confirm text for links.  Useful for templates and systems that want to use that system without actually
+		// using the smarty function.
+		jQuery('body').on('click', 'a.confirmlink', function(){
+			var confirmtext = $(this).attr('confirm'),
+				confirmhref = $(this).attr('href');
+			if(!confirmtext) confirmtext = 'Are you sure?';
+
+			if(confirm(confirmtext)){
+				Core.PostURL(confirmhref);
+			}
+
+			return false;
+		});
+
+		/**
+		 * Just a simple script to spruce up the message-* blocks with a bit of flair.
+		 */
+		var types = [
+				{ class: 'error',    icon: 'exclamation-sign' },
+				{ class: 'info',     icon: 'info-sign' },
+				{ class: 'note',     icon: 'asterisk' },
+				{ class: 'success',  icon: 'ok-sign' },
+				{ class: 'tutorial', icon: 'question-sign' }
+			],
+			i;
+
+		for(i in types){
+			jQuery('.message-' + types[i].class).each(function(){
+				jQuery(this).prepend('<span class="message-background-icon"><i class="icon-' + types[i].icon + '"></i></span>');
+			});
+		}
 	}
 
 })();
