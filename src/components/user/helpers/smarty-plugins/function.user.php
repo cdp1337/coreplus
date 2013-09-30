b824e8e24e60312cd374051b4c7352cb59dd1bba
Charlie Powell <charlie@eval.bz>
2013-09-29 23:04:24 -0400

    New smarty template, {user}.
    This allows the username to be displayed in the template when only the user id is known.

---
+++ src/components/user/helpers/smarty-plugins/function.user.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Convert a user id to a username and display that to the template.
+ *
+ * @param $params
+ * @param $template
+ *
+ * @return string
+ * @throws SmartyException
+ */
+function smarty_function_user($params, $template){
+	if(!isset($params['user'])){
+		throw new SmartyException('Required parameter [user] not provided for user!');
+	}
+
+	// Defaults
+	$userid = $params['user'];
+
+	$user = User::Construct($userid);
+
+	if(!$user) return '';
+
+	$username = $user->getDisplayName();
+	return $username;
+}
