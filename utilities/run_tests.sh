537e51af6db9b5c98dd4f4d060839fc3451d37f0
Charlie Powell <charlie@eval.bz>
2013-10-21 01:51:41 -0400

    Add the ability to execute a specific test file via utilities/run_tests.sh
    Also added some inline documentation for the script.

--- utilities/run_tests.sh
+++ utilities/run_tests.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+#
+# Simple script to run a collection of tests on the site with PHPUnit.
+#
+# This is NOT used by ant, but instead is useful for the developer when writing the tests,
+# as it can execute a specific test without waiting for the others to complete.
+#
+# Usage:
+#
+# run_tests.sh (no arguments)
+#    - Run every test suite on the system, mimicking what the ant utility does.
+#
+# run_tests.sh core
+#    - Run all the *core* tests
+#
+# run_tests.sh name_of_component
+#    - Run all tests in the "name_of_component" component
+#
+# run_tests.sh src/path/to/test/SomethingToTest.php
+#    - Run the specific test file only
 
 BASEDIR="$(readlink -f $(dirname $0)/..)"
 ROOTPDIR="$(readlink -f $(dirname $0)/../src)"
@@ -9,7 +28,6 @@ EXEC="$(which phpunit)"
 
 
 OPTS="--colors"
-#OPTS="--debug --verbose"
 
 
 function perform_test (){
@@ -25,11 +43,13 @@ function perform_test (){
 
 # If a specific component is requested, run the tests on only that location.
 if [ "$1" != "" ]; then
-	# Single always gets remapped to verbose.
-	#OPTS="--debug --verbose"
-	OPTS="--debug --colors"
+	# Tack on debug mode
+	OPTS="$OPTS --debug"
 
-	if [ "$1" == "core" ]; then
+	# Is this a specific file?
+	if [ -e "$BASEDIR/$1" ]; then
+		perform_test "$BASEDIR/$1"
+	elif [ "$1" == "core" ]; then
 		perform_test "$ROOTPDIR/core"
 	else
 		perform_test "$ROOTPDIR/components/$1"
