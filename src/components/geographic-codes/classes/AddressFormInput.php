86683bafe1c2294e04a6c291545593e9413cf8c9
Charlie Powell <charlie@eval.bz>
2013-11-01 15:44:27 -0400

    Add the MaxMind ip-geolocation libraries for more accurate and faster geo loookups.

---
+++ src/components/geographic-codes/classes/AddressFormInput.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * File for class AddressForm definition in the coreplus project
+ * 
+ * @author Charlie Powell <charlie@eval.bz>
+ * @date 20131030.1729
+ * @copyright Copyright (C) 2009-2013  Author
+ * @license GNU Affero General Public License v3 <http://www.gnu.org/licenses/agpl-3.0.txt>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/agpl-3.0.txt.
+ */
+
+
+/**
+ * A short teaser of what AddressForm does.
+ *
+ * More lengthy description of what AddressForm does and why it's fantastic.
+ *
+ * <h3>Usage Examples</h3>
+ *
+ *
+ * @todo Write documentation for AddressForm
+ * <h4>Example 1</h4>
+ * <p>Description 1</p>
+ * <code>
+ * // Some code for example 1
+ * $a = $b;
+ * </code>
+ *
+ *
+ * <h4>Example 2</h4>
+ * <p>Description 2</p>
+ * <code>
+ * // Some code for example 2
+ * $b = $a;
+ * </code>
+ *
+ * 
+ * @author Charlie Powell <charlie@eval.bz>
+ *
+ */
+class AddressFormInput extends FormElement {
+
+}
