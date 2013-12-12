43553c35889875b9c62a5ee5c445883e0deef6cb
Charlie Powell <charlie@eval.bz>
2013-12-12 11:33:14 -0500

    Migrate pre-2.8.x user groups to the 2.8.x version.

--- src/core/upgrades/270_to_280_groups.php
+++ src/core/upgrades/270_to_280_groups.php
@@ -3,28 +3,34 @@
  * Upgrade script to convert all the JSON-encoded user groups and transpose them to the new UserUserGroup object.
  */
 
-$dataset = \Core\Datamodel\Dataset::Init()
-	->select(['id', 'groups'])
-	->table('user')
-	->execute();
+$fac = new ModelFactory('UserModel');
+$fac->where('groups != ');
 
-var_dump($dataset); die();
+while(($user = $fac->getNext())){
+	/** @var UserModel $user */
+	// datamodel backed users have the groups listed in their column "groups".
+	$g = json_decode($user->get('groups'), true);
+	if(!$g) $g = array();
 
+	$gids = [];
 
-// datamodel backed users have the groups listed in their column "groups".
-$g = json_decode($this->_getModel()->get('groups'), true);
-if(!$g) $g = array();
-
-// Failover, if the user's group is a flat array of integers and multisite mode is enabled,
-// convert that to an array of sites, 0 being the first (root), with that list of groups.
-// This is useful for a site that has existing users that then converts to multisite.
-if(Core::IsComponentAvailable('enterprise') && MultiSiteHelper::IsEnabled()){
-	if(isset($g[0]) && !is_array($g[0])){
-
-		$g = array(0 => $g);
-		$this->_getModel()->set('groups', json_encode($g));
-		// It'll get saved on save() (if called).
+	foreach($g as $gid){
+		if(is_array($gid)){
+			$gids = array_merge($gids, $gid);
+		}
+		else{
+			$gids[] = $gid;
+		}
 	}
+	$gids = array_unique($gids);
+
+	$user->setGroups($gids);
+	$user->save();
 }
 
-$this->_groups = $g;
+// Now wipe out this temporary column data.
+\Core\Datamodel\Dataset::Init()
+	->update(['groups' => ''])
+	->table('user')
+	->where('groups != ')
+	->execute();
