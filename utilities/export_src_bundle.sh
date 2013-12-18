85ee5ee5cbff41140a20af71ae5e71124131c749
Charlie Powell <charlie@eval.bz>
2013-12-18 18:07:25 -0500

    New src exporter, useful for general sites.

---
+++ utilities/export_src_bundle.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Script to export a production-oriented clone of this project.
+
+BASEDIR="$(readlink -f $(dirname $0)/..)"
+
+EXPORTDIR="$BASEDIR/exports/src-bundles"
+FULLEXPORTTGZ="$EXPORTDIR/site-src-$(date +%Y%m%d).tgz"
+
+if [ ! -e "$EXPORTDIR" ]; then
+	mkdir -p "$EXPORTDIR"
+fi
+
+# Tar up everything, this will be the full build.
+echo "Exporting full tarball..."
+tar -czf "$FULLEXPORTTGZ" -C "$BASEDIR/src/" \
+components \
+config/configuration.xml.ex config/.htaccess \
+core install themes htaccess.ex index.php
+echo "OK!  Created $FULLEXPORTTGZ"
