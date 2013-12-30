91531bfe23356f2cf9000aa0b0000ba6cb05d0d3
Charlie Powell <charlie@eval.bz>
2013-12-30 03:29:13 -0500

    Allow for short-hand calling of the {user} smarty function.
    Also added some documentation for the function.

--- src/core/libs/smarty-plugins/function.user.php
+++ src/core/libs/smarty-plugins/function.user.php
@@ -2,20 +2,34 @@
 /**
  * Convert a user id to a username and display that to the template.
  *
+ * @package Core\User
+ *
  * @param $params
  * @param $template
  *
  * @return string
  * @throws SmartyException
+ *
+ * <h3>Usage Example</h3>
+ *
+ * <p>Typical usage for the Smarty user function</p>
+ *
+ * <code lang="text/html">
+ * User Name: {user $userid}&lt;br/&gt;
+ * </code>
  */
 function smarty_function_user($params, $template){
-	if(!isset($params['user'])){
+	if(isset($params['user'])){
+		$userid = $params['user'];
+	}
+	elseif(isset($params[0])){
+		$userid = $params[0];
+	}
+	else{
 		throw new SmartyException('Required parameter [user] not provided for user!');
 	}
 
-	// Defaults
-	$userid = $params['user'];
-
+	/** @var UserModel $user */
 	$user = UserModel::Construct($userid);
 
 	if(!$user) return '';
