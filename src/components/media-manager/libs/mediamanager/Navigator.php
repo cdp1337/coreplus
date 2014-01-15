1077da3ba5b980e629ed4b278ebb8995313f08a7
Charlie Powell <charlie@eval.bz>
2014-01-14 21:07:08 -0500

    Documentation change, @copyright.

--- src/components/media-manager/libs/mediamanager/Navigator.php
+++ src/components/media-manager/libs/mediamanager/Navigator.php
@@ -5,7 +5,7 @@
  * @package MediaManager
  * @author Charlie Powell <charlie@eval.bz>
  * @date 20130603.1802
- * @copyright Copyright (C) 2009-2013  Author
+ * @copyright Copyright (C) 2009-2014  Charlie Powell
  * @license GNU Affero General Public License v3 <http://www.gnu.org/licenses/agpl-3.0.txt>
  *
  * This program is free software: you can redistribute it and/or modify
