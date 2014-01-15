688a22693dc681ca08d7aabefe2b2fdd8e2712d0
Charlie Powell <charlie@eval.bz>
2014-01-14 21:10:39 -0500

    Documentation change, @copyright.

--- src/core/libs/core/datamodel/Schema.php
+++ src/core/libs/core/datamodel/Schema.php
@@ -5,7 +5,7 @@
  * @package Core\Datamodel
  * @author Charlie Powell <charlie@eval.bz>
  * @date 20131022.1655
- * @copyright Copyright (C) 2009-2013  Author
+ * @copyright Copyright (C) 2009-2014  Charlie Powell
  * @license GNU Affero General Public License v3 <http://www.gnu.org/licenses/agpl-3.0.txt>
  *
  * This program is free software: you can redistribute it and/or modify
