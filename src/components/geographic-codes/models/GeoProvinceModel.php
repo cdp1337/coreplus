7a0393bcf3c3ca7012338e6345a5913a75a4bed7
Charlie Powell <charlie@eval.bz>
2014-01-14 21:06:35 -0500

    Documentation change, @copyright.

--- src/components/geographic-codes/models/GeoProvinceModel.php
+++ src/components/geographic-codes/models/GeoProvinceModel.php
@@ -4,7 +4,7 @@
  * 
  * @author Charlie Powell <charlie@eval.bz>
  * @date 20131009.1421
- * @copyright Copyright (C) 2009-2013  Author
+ * @copyright Copyright (C) 2009-2014  Charlie Powell
  * @license GNU Affero General Public License v3 <http://www.gnu.org/licenses/agpl-3.0.txt>
  *
  * This program is free software: you can redistribute it and/or modify
