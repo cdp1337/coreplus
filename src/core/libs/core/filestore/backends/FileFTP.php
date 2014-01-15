86e4fa49fd74a6cb9540e9366475e53c1f6f9fa4
Charlie Powell <charlie@eval.bz>
2014-01-14 21:12:56 -0500

    Documentation change, @copyright.

--- src/core/libs/core/filestore/backends/FileFTP.php
+++ src/core/libs/core/filestore/backends/FileFTP.php
@@ -5,7 +5,7 @@
  * @package Core\Filestore\Backends
  * @author Charlie Powell <charlie@eval.bz>
  * @date 20130530.1917
- * @copyright Copyright (C) 2009-2013  Author
+ * @copyright Copyright (C) 2009-2014  Charlie Powell
  * @license GNU Affero General Public License v3 <http://www.gnu.org/licenses/agpl-3.0.txt>
  *
  * This program is free software: you can redistribute it and/or modify
--- src/core/libs/core/search/Helper.php
+++ src/core/libs/core/search/Helper.php
@@ -5,7 +5,7 @@
  * @package Core\Search
  * @author Charlie Powell <charlie@eval.bz>
  * @date 20131126.1744
- * @copyright Copyright (C) 2009-2013  Author
+ * @copyright Copyright (C) 2009-2014  Charlie Powell
  * @license GNU Affero General Public License v3 <http://www.gnu.org/licenses/agpl-3.0.txt>
  *
  * This program is free software: you can redistribute it and/or modify
