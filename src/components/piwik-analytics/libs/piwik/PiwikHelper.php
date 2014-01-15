8e55c498f1c80709c5d9bd0cb41a87f1ac02bf62
Charlie Powell <charlie@eval.bz>
2014-01-14 21:07:36 -0500

    Documentation change, @copyright.

--- src/components/piwik-analytics/libs/piwik/PiwikHelper.php
+++ src/components/piwik-analytics/libs/piwik/PiwikHelper.php
@@ -5,7 +5,7 @@
  * @package Piwik
  * @author Charlie Powell <charlie@eval.bz>
  * @date 20130619.0232
- * @copyright Copyright (C) 2009-2013  Author
+ * @copyright Copyright (C) 2009-2014  Charlie Powell
  * @license GNU Affero General Public License v3 <http://www.gnu.org/licenses/agpl-3.0.txt>
  *
  * This program is free software: you can redistribute it and/or modify
