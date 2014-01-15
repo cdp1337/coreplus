1dafa82f5e80f991bea982e801011bd6ed272474
Charlie Powell <charlie@eval.bz>
2014-01-14 21:09:48 -0500

    Documentation change, @copyright.

--- src/core/forms/FormLicenseInput.php
+++ src/core/forms/FormLicenseInput.php
@@ -5,7 +5,7 @@
  * @author Charlie Powell <charlie@eval.bz>
  * @date 20130716.1408
  * @package Core\Forms
- * @copyright Copyright (C) 2009-2013  Author
+ * @copyright Copyright (C) 2009-2014  Charlie Powell
  * @license GNU Affero General Public License v3 <http://www.gnu.org/licenses/agpl-3.0.txt>
  *
  * This program is free software: you can redistribute it and/or modify
