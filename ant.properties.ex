90d03b89d095b55eb7a63dc82142d056042edb38
Charlie Powell <charlie@eval.bz>
2014-01-16 16:52:10 -0500

    New data import script, useful for on-going development on sites.

--- ant.properties.ex
+++ ant.properties.ex
@@ -6,6 +6,11 @@
 # It's default to the directory the build.xml file is located in.
 
 
+
+###############################################################################
+####                        BASIC SITE SETTINGS                            ####
+###############################################################################
+
 # Rewrite base for the site.
 #
 # If this is currently installed in /~userblah/coreplus, set this to
@@ -15,9 +20,24 @@
 rewritebase=/src/
 
 
+# Controls if the site is in DEVELOPMENT mode or not.
+#
+# Setting this to "false" will be equivalent to production and "true" will be development.
+#
+# Gets copied to the config/configuration.xml file.
+devmode=false
+
+
+
+###############################################################################
+####                      LOCAL DATABASE SETTINGS                          ####
+###############################################################################
+
+
 # Database server name or IP address
 # Gets copied to the config/configuration.xml file.
 db.server=localhost
+
 
 # Database port
 # Gets copied to the config/configuration.xml file.
@@ -44,12 +64,6 @@ db.user=coreplus
 db.pass=coreplus
 
 
-# Controls if the site is in DEVELOPMENT mode or not.
-#
-# Setting this to "false" will be equivalent to production and "true" will be development.
-#
-# Gets copied to the config/configuration.xml file.
-devmode=false
 
 
 # Upstream directory
@@ -57,3 +71,36 @@ devmode=false
 # If you have Core Plus checked out locally, enter the full path here.
 # This is used by the sync_from_upstream.sh script for importing changes in Core and the core components.
 upstream=
+
+
+
+###############################################################################
+####                        DATA IMPORT SETTINGS                           ####
+###############################################################################
+####
+#### All of these settings control how the utilities/import-data.sh script works.
+#### If you do not intend to use this script, these settings are optional.
+####
+#### This is useful for development or demo environments where data should be imported from another source.
+
+
+# Server IP or hostname used to retrieve the database backup.
+data.import.production.host=
+
+
+# Server username used to retrieve the database backup.
+data.import.production.user=
+
+
+# Server port number for SSH used to retrieve the database backup.
+data.import.production.port=22
+
+
+# Directory on the server that contains the database backup.
+# MUST end with a trailing slash.
+data.import.production.datadir=/home/example/backups/
+
+
+# Any developer-specific operations to perform after import.
+# This file must be located inside the data/ directory of the project.
+data.import.custom.datafile=
---
+++ utilities/import-data.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# Script to import data from a production database to local.
+#
+
+
+# Core Plus base directory
+BASEDIR="$(readlink -f $(dirname $0)/..)"
+# The root directory, relative to the script
+HERE="$(dirname $0)"
+
+echo "Loading basescript..."
+source "/opt/eval/basescript.sh"
+
+
+# Production settings.
+HOST="$(egrep '^data\.import\.production\.host=' "$BASEDIR/ant.properties" | sed 's:^[^=]*=\(.*\):\1:')"
+USER="$(egrep '^data\.import\.production\.user=' "$BASEDIR/ant.properties" | sed 's:^[^=]*=\(.*\):\1:')"
+PORT="$(egrep '^data\.import\.production\.port=' "$BASEDIR/ant.properties" | sed 's:^[^=]*=\(.*\):\1:')"
+DIR="$(egrep '^data\.import\.production\.datadir=' "$BASEDIR/ant.properties" | sed 's:^[^=]*=\(.*\):\1:')"
+DEVDATA="$(egrep '^data\.import\.custom\.datafile=' "$BASEDIR/ant.properties" | sed 's:^[^=]*=\(.*\):\1:')"
+
+# Local settings.
+DBHOST="$(egrep '^db\.server=' "$BASEDIR/ant.properties" | sed 's:^[^=]*=\(.*\):\1:')"
+DBUSER="$(egrep '^db\.user=' "$BASEDIR/ant.properties" | sed 's:^[^=]*=\(.*\):\1:')"
+DBPASS="$(egrep '^db\.pass=' "$BASEDIR/ant.properties" | sed 's:^[^=]*=\(.*\):\1:')"
+DBNAME="$(egrep '^db\.name=' "$BASEDIR/ant.properties" | sed 's:^[^=]*=\(.*\):\1:')"
+#DBPORT="$(egrep '^db\.port=' "$BASEDIR/ant.properties" | sed 's:^[^=]*=\(.*\):\1:')"
+
+
+
+# Get the newest version from the remote server.
+#REMOTESQLGZ="$(ssh $HOST \"ls -c $HOSTSRC/*.sql.gz \| head -n1\")"
+#REMOTESQLGZ="$(ssh $HOST \'ls -c \"$HOSTSRC/*.sql.gz\"\')"
+REMOTESQLGZ="$(ssh -p$PORT $USER@$HOST ls -c $DIR*.sql.gz \| head -n1)"
+if [ -z "$REMOTESQLGZ" ]; then
+	echo "No *.sql.gz file located within $DIR on host $HOST."
+	exit 1
+fi
+
+if [ -e "$BASEDIR/data/data-latest.sql.gz" ]; then
+	# Check and see if I need to re-download this file.
+	REMOTEMD5="$(ssh -p$PORT $USER@$HOST md5sum $REMOTESQLGZ \| sed \'s: .*$::\')"
+	LOCALMD5="$(md5sum "$BASEDIR/data/data-latest.sql.gz" | sed 's: .*$::')"
+	if [ "$REMOTEMD5" == "$LOCALMD5" ]; then
+		DOWNLOADIT=0
+	else
+		DOWNLOADIT=1
+	fi
+else
+	DOWNLOADIT=1
+fi
+
+
+if [ "$DOWNLOADIT" == "1" ]; then
+	# Get that file and download it to "newest"
+	download ssh://$HOST:$REMOTESQLGZ "$BASEDIR/data/data-latest.sql.gz" -P $PORT -u $USER -o
+	#scp $HOST:$REMOTESQLGZ $HERE/data-newest.sql.gz
+	if [ "$?" != "0" ]; then
+		echo "something bad happened"
+		exit 1
+	fi
+fi
+
+printheader "Importing database..."
+gunzip "$BASEDIR/data/data-latest.sql.gz" -c | mysql -u$DBUSER -p"$DBPASS" $DBNAME
+checkexitstatus "$?"
+
+# Run any upgrades queued up for the "next" version of production.
+if [ -e "$BASEDIR/data/prod-next-upgrade.sql" ]; then
+	printheader "Applying production upgrade patch..."
+	mysql -u$DBUSER -p"$DBPASS" $DBNAME < "$BASEDIR/data/prod-next-upgrade.sql"
+	checkexitstatus "$?"
+fi
+
+# Run any production-to-development data.
+if [ -e "$BASEDIR/data/prod-to-dev.sql" ]; then
+	printheader "Applying production-to-development patch..."
+	mysql -u$DBUSER -p"$DBPASS" $DBNAME < "$BASEDIR/data/prod-to-dev.sql"
+	checkexitstatus "$?"
+fi
+
+# Run any developer-specific file specified in the ant.properties.
+if [ -e "$BASEDIR/data/$DEVDATA" ]; then
+	printheader "Applying developer-specific $DEVDATA patch..."
+	mysql -u$DBUSER -p"$DBPASS" $DBNAME < "$BASEDIR/data/$DEVDATA"
+	checkexitstatus "$?"
+fi
