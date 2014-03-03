0b74ca53138e203c089bf6a394b48736ee9d480a
Nicholas Hinsch <nicholas@eva.bz>
2014-03-03 11:44:06 -0500

    form element background tweak for option groups (checkboxes or radio)

--- src/core/bootstrap.compiled.php
+++ src/core/bootstrap.compiled.php
@@ -15,7 +15,7 @@
  * @copyright Copyright (C) 2009-2014  Charlie Powell
  * @license     GNU Affero General Public License v3 <http://www.gnu.org/licenses/agpl-3.0.txt>
  *
- * @compiled Mon, 03 Mar 2014 10:49:14 -0500
+ * @compiled Mon, 03 Mar 2014 11:36:55 -0500
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
