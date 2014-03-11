b2aa51bdacf34268d9ea09abd7c4d9cda318db67
Charlie Powell <charlie@eval.bz>
2014-03-11 13:38:53 -0400

    Include the profiler in the utility reinstall.

--- utilities/reinstall.php
+++ utilities/reinstall.php
@@ -63,6 +63,8 @@ require_once(ROOT_PDIR . 'install/classes/InstallerStep.php');
 require_once(ROOT_PDIR . 'core/functions/Core.functions.php');
 require_once(ROOT_PDIR . 'core/libs/core/utilities/logger/functions.php');
 require_once(ROOT_PDIR . 'install/utilities.php');
+require_once(ROOT_PDIR . 'core/libs/core/utilities/profiler/Profiler.php');
+require_once(ROOT_PDIR . 'core/libs/core/utilities/logger/functions.php');
 
 require_once(ROOT_PDIR . "core/libs/core/ConfigHandler.class.php");
 
