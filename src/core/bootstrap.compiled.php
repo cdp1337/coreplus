e997eb08c9ae9cf980166ad56d57636d72d32a1e
Charlie Powell <charlie@eval.bz>
2014-04-02 04:53:14 -0400

    Release Core Plus 3.2.1
    Signed-off-by: Charlie Powell <charlie@eval.bz>

--- src/core/bootstrap.compiled.php
+++ src/core/bootstrap.compiled.php
@@ -15,7 +15,7 @@
  * @copyright Copyright (C) 2009-2014  Charlie Powell
  * @license     GNU Affero General Public License v3 <http://www.gnu.org/licenses/agpl-3.0.txt>
  *
- * @compiled Wed, 02 Apr 2014 04:49:58 -0400
+ * @compiled Wed, 02 Apr 2014 04:52:32 -0400
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
