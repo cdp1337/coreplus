4900a7f4f750be6e2df30d718cbd076fcc913202
Charlie Powell <charlie@eval.bz>
2014-05-15 13:56:48 -0400

    Rewrite the controls logic to behave better with overflow:hidden.

--- src/core/assets/js/core.context-controls.js
+++ src/core/assets/js/core.context-controls.js
@@ -1,15 +1,23 @@
 $(function(){
+	var $body = $('body'),
+		defaults = {
+			proxyicon: 'cog',
+			proxytext: 'Controls',
+			proxyforce: false
+		},
+		$currentopen = null,
+		$currentover = null,
+		timer = null;
+
 	//$('.controls-hover').each(function(){
 	$('.controls').each(function(){
+			// The original menu, this will become contextual.
 		var $original = $(this),
+			// A clone of it, this will be flattened and act as the anchor.
 			$clone = $original.clone(),
-			$wrapper = $('<div style="position:relative;" class="controls-hover-wrapper"/>'),
+			// The main wrapper
+			//$wrapper = $('<div class="controls-hover-wrapper"/>'),
 			controlcount = $clone.find('li').length,
-			defaults = {
-				proxyicon: 'cog',
-				proxytext: 'Controls',
-				proxyforce: false
-			},
 			options = {
 				proxyicon: $original.data('proxy-icon'),
 				proxytext: $original.data('proxy-text'),
@@ -23,14 +31,17 @@ $(function(){
 		}
 
 
-		$original.addClass('controls-hover').after($wrapper);
 
-		// Now, I can move them both into the wrapper.
-		$original.appendTo($wrapper);
-		$clone.appendTo($wrapper);
+		// Append the clone to the end of the original's parent.
+		// This should be in the same spot, if it isn't for some reason,
+		// wrap your <ul class="controls"/> with an empty <div/>.
+		$original.parent().append($clone);
+
+		// And move the original to the end of the body.
+		$body.append($original);
 
 		// I'm going to do modifications to the original, since that may have events already bound to it.
-		$original.addClass('context-menu');
+		$original.addClass('context-menu').addClass('controls-hover');
 
 		// This helps fine-tune the styles a little.
 		if(controlcount == 1){
@@ -41,7 +52,7 @@ $(function(){
 		}
 
 		// Ensure the clone width is correct.
-		$original.css('min-width', $wrapper.width());
+		$original.css('min-width', $clone.width());
 
 		// And hide it!  (it'll be shown on hover)
 		$original.hide();
@@ -51,12 +62,52 @@ $(function(){
 			$clone.html('<li><i class="icon-' + options.proxyicon + '"></i>' + (options.proxytext ? ' ' + options.proxytext : '') + '</li>');
 		}
 
-		$wrapper
-			.mouseover(function(){
-				$original.show();
+		$clone
+			.mouseenter(function(){
+				// Show this element's companion context menu always when the mouse enters its space.
+				var o = $clone.offset();
+
+				if($currentopen){
+					$currentopen.hide();
+				}
+
+				$original.css(
+					{
+						top: o.top + 'px',
+						left: (o.left + $clone.innerHeight()) + 'px'
+					}
+				).show();
+
+				$currentopen = $original;
+				$currentover = $clone;
 			})
-			.mouseout(function(){
-				$original.hide();
+			.mouseleave(function(){
+				if(timer){
+					clearTimeout(timer);
+				}
+				timer = setTimeout(function(){
+					if($currentover == null && $currentopen){
+						$currentopen.fadeOut();
+					}
+				}, 500);
+
+				$currentover = null;
+			});
+
+		$original
+			.mouseenter(function(){
+				$currentover = $original;
+			})
+			.mouseleave(function(){
+				$currentover = null;
+				if(timer){
+					clearTimeout(timer);
+				}
+				timer = setTimeout(function(){
+					if($currentover == null && $currentopen){
+						$currentopen.fadeOut();
+					}
+				}, 500);
 			});
 	});
 });
--- src/core/libs/smarty-plugins/function.controls.php
+++ src/core/libs/smarty-plugins/function.controls.php
@@ -1,9 +1,28 @@
 <?php
-
 /**
  * Function responsible for displaying a <ul/> of control links for a given object.
  *
  * Will call the /core/controllinks/{baseurl} hook automatically to retrieve any addon calls.
+ *
+ * <h3>Example Usage</h3>
+ *
+ * <pre><code>{controls baseurl="/user/view" subject="`$user.id`"}</code></pre>
+ * <p>
+ * Traditional usage of the controls and the /core/controllinks hook.
+ * </p>
+ *
+ * <pre><code>{controls model=$user}</code></pre>
+ * <p>
+ * Shortened, inline version of the model controls and the /core/controllinks hook.
+ * </p>
+ * <p>
+ * This version will first query the Model's getControlLinks method,
+ * then the appropriate /core/controllinks hook for any additional links.
+ * </p>
+ * <p>
+ * This enables a shorter version of the control hook system for application developers,
+ * since they no longer need to use a hook call for the base links.
+ * </p>
  *
  * @param Array  $params Associative array of parameters passed in
  *               'baseurl' REQUIRED The "baseurl" of this controllink object.
@@ -13,27 +32,40 @@
  * @throws SmartyException
  */
 function smarty_function_controls($params, $template){
-	// The params here are baseurl which is used by the hook system, and
-	// the subject
 
-	// {controls baseurl="/user/view" subject="`$user.id`"}
+	if(isset($params['model'])){
+		$subject = $params['model'];
+		if(!$subject instanceof Model){
+			throw new SmartyException('Only Models can be used with the {controls model=...} syntax!');
+		}
+		$baseurl = '/' . strtolower(get_class($subject));
+	}
+	elseif(isset($params['baseurl'])){
+		$baseurl = $params['baseurl'];
 
-	if(!isset($params['baseurl'])){
-		throw new SmartyException('Unable to get links without a baseurl');
+		// They may or may not have subjects.
+		// The subject is the subject matter of this control link.
+		$subject = (isset($params['subject'])) ? $params['subject'] : null;
+	}
+	else{
+		throw new SmartyException('Unable to get links without a baseurl!');
 	}
 
-	$baseurl = $params['baseurl'];
+	// Hover should be the default behaviour.
+	if(isset($params['hover'])){
+		$hover = ($params['hover']);
+	}
+	else{
+		$hover = true;
+	}
 
-	// They may or may not have subjects.
-	// The subject is the subject matter of this control link.
-	$subject = (isset($params['subject'])) ? $params['subject'] : null;
+	$firstlinks = ($subject instanceof Model) ? $subject->getControlLinks() : [];
+	$additionallinks = HookHandler::DispatchHook('/core/controllinks' . $baseurl, $subject);
 
-	$links = HookHandler::DispatchHook('/core/controllinks' . $baseurl, $subject);
+	$links = array_merge($firstlinks, $additionallinks);
 
 	$controls = new ViewControls();
-	if(isset($params['hover'])){
-		$controls->hovercontext = true;
-	}
+	$controls->hovercontext = $hover;
 	$controls->addLinks($links);
 
 	echo $controls->fetch();
