5ca6d9cc6b0195041576574efe2b32b97b67f86c
Charlie Powell <charlie@eval.bz>
2014-09-29 00:25:26 -0400

    Documentation work

--- src/core/libs/smarty-plugins/block.a.php
+++ src/core/libs/smarty-plugins/block.a.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * File for the smarty "a" block function
+ *
  * @package Core
  * @since 1.9
  * @author Charlie Powell <charlie@eval.bz>
@@ -20,7 +22,49 @@
  */
 
 /**
- * Return a valid resolved a tag, with whatever inner content preserved.
+ * Return a valid resolved "A" tag, with whatever inner content preserved.
+ *
+ * This is the recommended way to handle &lt;a/&gt; tags in Core templates.
+ * The href attribute is automatically resolved to the primary rewrite URL,
+ * and all additional links, (mostly), are passed through to the template as-is.
+ *
+ * #### Smarty Parameters
+ *
+ *  * assign
+ *    * Assign the result to a variable instead of printing it to stdout.
+ *  * confirm
+ *    * Set to a string to prompt the user with the string before submitting the link via a POST request.
+ *  * history
+ *    * Set to a number, (1, 2, etc), to set the href to that user's last nth page from history.
+ *
+ * #### Standard Example
+ *
+ * <pre>
+ * // The smarty line
+ * {a href="/content/view/1"}Something Blah{/a}
+ * // Resolves to
+ * &lt;a href="/homepage"&gt;Something Blah&lt;/a&gt;
+ * </pre>
+ *
+ * #### External Link
+ *
+ * Also works with already-resolved or external links
+ *
+ * <pre>
+ * {a href="http://corepl.us" target="_blank"}Link Somewhere!{/a}
+ * // Resolves to
+ * &lt;a href="http://corepl.use" target="_blank"&gt;Link Somewhere!&lt;/a&gt;
+ * </pre>
+ *
+ * #### Multisite Links
+ *
+ * When multi-site is installed and activated, cross-site links can also be used that resolve based on that site's criteria.
+ *
+ * <pre>
+ * {a href="site:12/content/view/5"}Child #12 About Page{/a}
+ * // Resolves to
+ * &lt;a href="http://child-12-url.example.com/about-us"&gt;Child #12 About Page&lt;/a&gt;
+ * </pre>
  *
  * @param $params array
  * @param $innercontent string
@@ -42,6 +86,7 @@ function smarty_block_a($params, $innercontent, $template, &$repeat){
 	// This has the cool ability of not requiring jquery to run, since it is all handled with PHP logic.
 	if(isset($params['confirm'])){
 		$params['onclick'] = 'return Core.ConfirmEvent(this);';
+		// @todo Convert these to the data- prefix instead of data:
 		$params['data:href'] = Core::ResolveLink($params['href']);
 		$params['data:confirm'] = $params['confirm'];
 		//$params['onClick'] = "if(confirm('" . str_replace("'", "\\'", $params['confirm']) . "')){ Core.PostURL('" . str_replace("'", "\\'", Core::ResolveLink($params['href'])) . "'); } return false;";
