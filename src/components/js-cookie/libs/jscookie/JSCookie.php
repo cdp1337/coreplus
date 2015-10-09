d056fd1fd31f3d3ebea01528219c176e3c79d5fc
Charlie Powell <charlie@evalagency.com>
2015-10-09 17:51:43 -0400

    Add new component, js.cookie!
    jquery.cookie is no longer maintained, so this is the preferred way of pulling cookies from JS.

---
+++ src/components/js-cookie/libs/jscookie/JSCookie.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @todo      Enter a meaningful file description here!
+ *
+ * @author    Charlie Powell <charlie@evalagency.com>
+ * @date      20151009.1732
+ * @copyright Copyright (C) 2009-2015  Charlie Powell
+ * @license   Released under the MIT license
+ */
+
+namespace JSCookie;
+
+class JSCookie {
+	public static function IncludeJS () {
+		\Core\view()->addScript('assets/js/js-cookie/js.cookie.js', 'head');
+		return true;
+	}
+}
