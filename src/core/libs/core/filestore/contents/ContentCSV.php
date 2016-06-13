6571b3e63888441a1e79420e5d47a5a320b65d63
Charlie Powell <charlie@evalagency.com>
2016-06-13 10:20:03 -0400

    New functionality on ContentCSV.
    ContentCSV can now parse a document without loading the entire file into memory at once via parseChunk.
    Also fixes in the hasHeader autodetection.
    New method to get the total number of lines in the CSV file, (this feature is still buggy as it relies on `wc`).

--- src/core/libs/core/filestore/contents/ContentCSV.php
+++ src/core/libs/core/filestore/contents/ContentCSV.php
@@ -69,6 +69,11 @@ class ContentCSV implements Filestore\Contents {
 	 * @var array
 	 */
 	private $_header = null;
+	
+	private $_readerHandle = null;
+	
+	/** @var null|int Total lines (minus header) of this file, generated dynamically from getTotalLines */
+	private $_totalLines = null;
 
 	/**
 	 * The original file object
@@ -79,7 +84,16 @@ class ContentCSV implements Filestore\Contents {
 		$this->_file = $file;
 	}
 
-	public function parse($delimiter = ','){
+	/**
+	 * Read an entire file into memory as an associative or indexes array.
+	 * 
+	 * @param string   $delimiter
+	 * @param int|null $lines     Set to an int to limit that number of lines to be returned.
+	 *
+	 * @return array
+	 * @throws \Exception
+	 */
+	public function parse($delimiter = ',', $lines = null){
 		$filename = $this->_file->getFilename();
 
 		if(!$this->_file->exists()){
@@ -98,9 +112,14 @@ class ContentCSV implements Filestore\Contents {
 		}
 
 		$data = array();
+		
+		// Read 1 more line than requested if there's a header.
+		if($lines !== null && $hasheader){
+			$lines ++;
+		}
 
 		$i = 0;
-		while(!feof($fh)){
+		while(!feof($fh) && ($lines === null || $i < $lines)){
 			$record = fgetcsv($fh, 1024, $delimiter);
 			$i++;
 
@@ -126,6 +145,88 @@ class ContentCSV implements Filestore\Contents {
 		return $data;
 	}
 
+
+	/**
+	 * Read up to N lines of a file into memory as an associative or indexes array.
+	 * 
+	 * Useful for large files!
+	 *
+	 * @param string $delimiter
+	 * @param int    $lines
+	 *
+	 * @return array|false
+	 * @throws \Exception
+	 */
+	public function parseChunked($delimiter = ',', $lines = 100){
+		$filename = $this->_file->getFilename();
+		$firstRead = false;
+
+		if(!$this->_file->exists()){
+			throw new \Exception('Unable to open ' . $filename . ' for parsing, does not exist!');
+		}
+
+		// Some CSV files may ship with a header line.
+		$hasheader = $this->hasHeader();
+		// Here, I just need the keys.
+		$header = array_keys($this->getHeader());
+		
+		// Try to get a lock on the file handle.
+		if($this->_readerHandle === null){
+			$this->_readerHandle = fopen($filename, 'r');
+			if(!$this->_readerHandle){
+				throw new \Exception('Unable to lock ' . $filename . ' for parsing!');
+			}
+			$firstRead = true;
+		}
+		
+		if($this->_readerHandle === false){
+			// Return false to indicate it's the end of the file and reset to allow this running again.
+			$this->_readerHandle = null;
+			return false;
+		}
+
+		$data = array();
+
+		// Read 1 more line than requested if there's a header.
+		if($lines !== null && $hasheader && $firstRead){
+			$lines ++;
+		}
+
+		$i = 0;
+		while(!feof($this->_readerHandle) && $i < $lines){
+			$record = fgetcsv($this->_readerHandle, 1024, $delimiter);
+			$i++;
+
+			// End of file?
+			if(!$record){
+				fclose($this->_readerHandle);
+				$this->_readerHandle = false;
+				break;
+			}
+
+			if($i == 1 && $hasheader && $firstRead){
+				// Skip the header if there is one.
+				continue;
+			}
+			elseif($hasheader){
+				// A header is present and set, associate this record!
+				$data[] = array_combine($header, $record);
+			}
+			else{
+				// Just a plain'ol indexed record.
+				$data[] = $record;
+			}
+		}
+		
+		// End of file?  Cleanup.
+		if($this->_readerHandle && feof($this->_readerHandle)){
+			fclose($this->_readerHandle);
+			$this->_readerHandle = false;
+		}
+
+		return $data;
+	}
+
 	/**
 	 * Actively do a simple check with heuristics on this file and see if it has a header.
 	 * (of just return the explicitly set value)
@@ -134,46 +235,11 @@ class ContentCSV implements Filestore\Contents {
 	 * @throws \Exception
 	 */
 	public function hasHeader(){
-		if($this->_hasheader === null){
+		if($this->_hasheader === null) {
 			// Autodetect!
-
 			$line = $this->getHeader();
-
-			// If there are duplicate records present, it's probably not a header.
-			$counts = array_values(array_count_values($line));
-			sort($counts); // Sort them ascending, (biggest number at the end)
-			if(array_pop($counts) > 1){
-				$this->_hasheader = false;
-				return false;
-			}
-
-			// Do some heuristics on each individual record.
-			foreach($line as $val){
-				// If there is an empty column, it's probably not a header.
-				if(!trim($val)){
-					$this->_hasheader = false;
-					return false;
-				}
-				// If it's a number, it's probably not a header.
-				if(is_int($val)){
-					$this->_hasheader = false;
-					return false;
-				}
-				// If it's a date, it's probably not a header.
-				if(preg_match('/[0-9]+[\/\-][0-9]+[\/\-][0-9]+/', $val)){
-					$this->_hasheader = false;
-					return false;
-				}
-			}
-
-			// None of the scans detected that it's a value... maybe.
-			$this->_hasheader = true;
-			return true;
 		}
-		else{
-			// The user, (or this method), set it... yay/nay.
-			return $this->_hasheader;
-		}
+		return $this->_hasheader;
 	}
 
 	/**
@@ -204,26 +270,23 @@ class ContentCSV implements Filestore\Contents {
 			$counts = array_values(array_count_values($line));
 			sort($counts); // Sort them ascending, (biggest number at the end)
 			if(array_pop($counts) > 1){
-				$this->_hasheader = false;
-				return false;
+				$this->_header = [];
+				$line = [];
 			}
 
 			// Do some heuristics on each individual record.
 			foreach($line as $val){
 				// If there is an empty column, it's probably not a header.
 				if(!trim($val)){
-					$this->_hasheader = false;
-					return false;
+					$this->_header = [];
 				}
 				// If it's a number, it's probably not a header.
 				if(is_int($val)){
-					$this->_hasheader = false;
-					return false;
+					$this->_header = [];
 				}
 				// If it's a date, it's probably not a header.
 				if(preg_match('/[0-9]+[\/\-][0-9]+[\/\-][0-9]+/', $val)){
-					$this->_hasheader = false;
-					return false;
+					$this->_header = [];
 				}
 			}
 
@@ -234,7 +297,41 @@ class ContentCSV implements Filestore\Contents {
 				$this->_header[$val] = $title;
 			}
 		}
+		
+		if($this->_hasheader === null){
+			$this->_hasheader = (sizeof($this->_header) > 0);
+		}
 
 		return $this->_header;
+	}
+
+	/**
+	 * Get the total number of data lines in this CSV.
+	 * 
+	 * If there is a header, omit that, so 1000 lines with a header returns 999.
+	 * 
+	 * @throws \Exception
+	 * 
+	 * @return int
+	 */
+	public function getTotalLines(){
+		if($this->_totalLines === null){
+			// WC is a LOT quicker than native PHP, so simply use that to calculate this.
+			exec('wc -l ' . escapeshellarg($this->_file->getFilename()), $out, $ret);
+			
+			if($ret !== 0){
+				throw new \Exception('Unable to calculate number of lines in requested file ' . $this->_file->getFilename() . ': ' . print_r($out, true));
+			}
+			
+			// This is in the format of "1234 filename-here.blah"
+			$c = (int)preg_replace('#^([0-9]*)\s.*#', '$1', $out[0]);
+			
+			if($this->hasHeader()){
+				$c--;
+			}
+			$this->_totalLines = $c;
+		}
+		
+		return $this->_totalLines;
 	}
 }
