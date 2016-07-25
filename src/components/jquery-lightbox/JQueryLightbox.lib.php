f2453f2c5fc23e81dd0c6eac0d7f289c74f75f99
Charlie Powell <charlie@evalagency.com>
2016-07-25 18:48:44 -0400

    New version of Lightbox from upstream.

--- src/components/jquery-lightbox/JQueryLightbox.lib.php
+++ src/components/jquery-lightbox/JQueryLightbox.lib.php
@@ -1,10 +1,6 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
- * User: powellc
- * Date: 7/11/12
- * Time: 3:16 PM
- * To change this template use File | Settings | File Templates.
+ * @license MIT
  */
 class JQueryLightbox {
 	public static function Load(){
@@ -13,15 +9,9 @@ class JQueryLightbox {
 		if(!JQuery::IncludeJQueryUI()){
 			return false;
 		}
-
-		if(ConfigHandler::Get('/core/javascript/minified')){
-			\Core\view()->addScript ('js/jquery.lightbox-0.5.min.js');
-		}
-		else{
-			\Core\view()->addScript ('js/jquery.lightbox-0.5.js');
-		}
-
-		\Core\view()->addStylesheet('css/jquery.lightbox-0.5.css');
+		
+		\Core\view()->addScript('js/lightbox.js', 'foot');
+		\Core\view()->addStylesheet('css/lightbox.css');
 
 		// IMPORTANT!  Tells the script that the include succeeded!
 		return true;
